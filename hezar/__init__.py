1b55ec80f57d3f8f9b7eff5c5e79f59f4856e0a4
arxyzan <arxyzan@gmail.com>
2022-09-14 19:01:50 +0430

    add registry for criterions and optimizers

--- hezar/__init__.py
+++ hezar/__init__.py
@@ -1,7 +1,9 @@
 import hezar.configs
 import hezar.data
 import hezar.models
+import hezar.tasks
 import hezar.utils
+from hezar.registry import criterions_registry, optimizers_registry
 from hezar.models import models_registry, register_model
 
 __version__ = '0.0.1'
