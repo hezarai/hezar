2f037a7b2d7cc58f9e5cfa578b3bfb59584d8893
arxyzan <arxyzan@gmail.com>
2023-02-19 13:04:56 +0330

    [hezar][models][lm] add [distilbert]

---
+++ hezar/models/lm/distilbert/distilbert_lm.py
@@ -0,0 +1,40 @@
+"""
+A DistilBERT Language Model (HuggingFace Transformers) wrapped by a Hezar Model class
+"""
+from transformers import DistilBertModel, DistilBertConfig
+
+from hezar.models import register_model, Model
+from hezar.data import Text
+from .distilbert_lm_config import DistilBertLMConfig
+
+
+@register_model("distilbert_lm", config_class=DistilBertLMConfig)
+class DistilBertLM(Model):
+    def __init__(self, config):
+        super().__init__(config=config)
+        self.model = self._build()
+
+    def _build(self):
+        config = DistilBertConfig(**self.config)
+        model = DistilBertModel(config)
+        return model
+
+    def forward(self, inputs, **kwargs):
+        outputs = self.model(**inputs, **kwargs)
+        return outputs
+
+    def preprocess(self, inputs: str, **kwargs):
+        invalid_chars = self.config.get("invalid_chars", [])
+        inputs = Text(inputs, tokenizer=self.tokenizer)
+        inputs = inputs.normalize().filter_out(invalid_chars).tokenize(return_tensors="pt")
+        return inputs
+
+    def postprocess(self, inputs, **kwargs):
+        hidden_states = inputs.get('hidden_states', None)
+        attentions = inputs.get('attentions', None)
+        outputs = {
+            'last_hidden_state': inputs.get('last_hidden_state'),
+            'hidden_states': hidden_states,
+            'attentions': attentions
+        }
+        return outputs
