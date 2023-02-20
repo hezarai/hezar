8d4cbad870aa8880a82abf1cc4e4f9b64ddb173a
arxyzan <arxyzan@gmail.com>
2023-02-20 17:05:12 +0330

    [hezar][examples] text_classification_example.py: minor

--- examples/text_classification_example.py
+++ examples/text_classification_example.py
@@ -1,8 +1,9 @@
-from hezar.models import Model
+from hezar.models import Model, build_model
 from hezar.preprocessors.tokenizer import Tokenizer
 
 path = "hezar-ai/distilbert-fa-sentiment-v1"
 model = Model.load(path)
+# model = build_model('distilbert_text_classification', id2label={0: 'negative', 1: 'positive'})
 tokenizer = Tokenizer.load(path)
 inputs = tokenizer(["یه تست خیلی خفن"], return_tensors="pt")
 outputs = model.predict(inputs)
