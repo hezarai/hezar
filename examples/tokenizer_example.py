5f192afdbc6a679817633ac94c686ba54f294f2a
arxyzan <arxyzan@gmail.com>
2023-02-27 11:27:59 +0330

    [hezar][examples] minor

--- examples/tokenizer_example.py
+++ examples/tokenizer_example.py
@@ -1,8 +1,6 @@
-from hezar.builders import build_preprocessor
 from hezar.preprocessors.tokenizer import Tokenizer
 
-
-preprocessor = Tokenizer.load(hub_or_local_path="hezar-ai/distilbert-fa-sentiment-v1")
-y1 = preprocessor(["hello guys", "my name is", "Aryan"], return_attention_mask=True, return_tensors="pt")
-...
+tokenizer = Tokenizer.load(hub_or_local_path="hezar-ai/distilbert-fa-sentiment-v1")
+encoded = tokenizer(["Hello guys", "my name is", "Aryan"], return_attention_mask=True)
+decoded = tokenizer.decode(encoded['token_ids'])
 
