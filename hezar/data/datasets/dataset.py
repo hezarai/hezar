a625761db3a501c970c347e5918e1e5fdabc468e
arxyzan <arxyzan@gmail.com>
2023-02-27 17:41:32 +0330

    [hezar] change self.preprocessor to self.tokenizer

--- hezar/data/datasets/dataset.py
+++ hezar/data/datasets/dataset.py
@@ -6,7 +6,7 @@ from ...configs import DatasetConfig
 class Dataset(TorchDataset):
     def __init__(self, config: DatasetConfig, **kwargs):
         self.config = config.update(kwargs)
-        self.preprocessor = None
+        self.tokenizer = None
 
     def __len__(self):
         ...
