0ac7bdbcae87d39ac07101a899bedefbd776152d
arxyzan <arxyzan@gmail.com>
2023-03-12 12:30:07 +0330

    Model: more explicit and precise state dict loading

--- hezar/models/model.py
+++ hezar/models/model.py
@@ -85,6 +85,17 @@ class Model(nn.Module):
         return model
 
     def load_state_dict(self, state_dict, **kwargs):
+        incompatible_keys = []
+        src_state_dict = self.state_dict()
+        for (src_weight_key, src_weight), (trg_key, trg_weight) in zip(src_state_dict.items(), state_dict.items()):
+            if src_weight.shape != trg_weight.shape:
+                incompatible_keys.append(src_weight_key)
+
+        missing_keys = []
+        diff = len(src_state_dict) - len(state_dict)
+        if diff > 0:
+            missing_keys.extend(list(src_state_dict.keys())[-diff:])
+
         try:
             super().load_state_dict(state_dict, strict=True)
         except RuntimeError:
@@ -92,6 +103,8 @@ class Model(nn.Module):
             logger.warning(
                 "Partially loading the weights as the model architecture and the given state dict are "
                 "incompatible! \nIgnore this warning in case you plan on fine-tuning this model"
+                f"Incompatible keys: {incompatible_keys}\n"
+                f"Missing keys: {missing_keys}\n"
             )
 
     def save(self, path: Union[str, os.PathLike], save_config: bool = True):
