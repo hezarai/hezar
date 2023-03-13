89b69ee13c13d4649b8de8c5969e1c979da1f256
arxyzan <arxyzan@gmail.com>
2023-03-13 23:52:41 +0330

    train_example: minor

--- examples/train_example.py
+++ examples/train_example.py
@@ -7,28 +7,29 @@ from hezar import (
     build_scheduler,
 )
 
+name = "distilbert_text_classification"
 
 dataset_config = {
     "path": "hezar-ai/sentiment_digikala_snappfood",
     "text_field": "text",
     "label_field": "label",
-    "tokenizer_path": "hezar-ai/distilbert-fa",
+    "tokenizer_path": "hezar-ai/distilbert-base-fa",
 }
 train_dataset = build_dataset(name="text_classification", split="train", **dataset_config)
 eval_dataset = build_dataset(name="text_classification", split="test", **dataset_config)
 
-model = build_model("distilbert_text_classification", id2label=train_dataset.id2label)
+model = build_model(name, id2label=train_dataset.id2label)
 optimizer = build_optimizer("adamw", model.parameters(), lr=2e-5)
 lr_scheduler = build_scheduler("reduce_on_plateau", optimizer=optimizer)
 
 train_config = TrainConfig(
-    name="distilbert_text_classification",
+    name=name,
     device="cuda",
-    init_weights_from="hezar-ai/distilbert-fa",
+    init_weights_from="hezar-ai/distilbert-base-fa",
     batch_size=16,
     num_train_epochs=10,
     checkpoints_dir="checkpoints/",
-    metrics=[("f1", {"task": "multiclass", "average": "micro"})],
+    metrics={"f1": {"task": "multiclass"}},
 )
 
 trainer = Trainer(
