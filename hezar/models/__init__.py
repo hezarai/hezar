47341dc1b6217ae07ecf685bbb22c58440306480
arxyzan <arxyzan@gmail.com>
2023-03-14 11:25:16 +0330

    add empty models: ocr and image captioning

--- hezar/models/__init__.py
+++ hezar/models/__init__.py
@@ -1,4 +1,6 @@
 from .model import *
-from .language_modeling import *
 from .dummy import *
+from .image_captioning import *
+from .language_modeling import *
+from .ocr import *
 from .text_classification import *
