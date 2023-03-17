a6821b2a7b1b96326442bc3b838b18f68da071a6
arxyzan <arxyzan@gmail.com>
2023-03-17 13:34:29 +0330

    fix tests

--- tests/test_builders.py
+++ tests/test_builders.py
@@ -5,15 +5,7 @@ def build_models():
     from hezar.registry import models_registry
 
     for name, module in models_registry.items():
-        model = build_model(name, config=module["config_class"]())
-        print(f"Succesfully built `{name}`")
-
-
-def build_datasets():
-    from hezar.registry import datasets_registry
-
-    for name, module in datasets_registry.items():
-        model = build_dataset(name, config=module["config_class"]())
+        model = build_model(name, config=module["config_class"](), num_labels=2)
         print(f"Succesfully built `{name}`")
 
 
@@ -27,5 +19,4 @@ def build_preprocessors():
 
 if __name__ == "__main__":
     build_models()
-    build_datasets()
     build_preprocessors()
