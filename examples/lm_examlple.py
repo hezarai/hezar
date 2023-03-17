38edada58061a3308535ec392182ac078d58c1e8
arxyzan <arxyzan@gmail.com>
2023-03-17 12:01:42 +0330

    examples: minor

--- examples/lm_examlple.py
+++ examples/lm_examlple.py
@@ -3,14 +3,14 @@ from hezar.models import Model
 from hezar.preprocessors import Tokenizer
 
 
-path = "hezar-ai/distilbert-fa"
+path = "hezar-ai/bert-base-fa"
 tokenizer = Tokenizer.load(path)
 text = tokenizer(["hello guys"], return_tensors="pt")
 
-model = build_model("distilbert_lm")
+model = build_model("bert_lm")
 outputs = model.predict(text, output_attentions=True, output_hidden_states=True)
 print(outputs)
 
-model = Model.load(path, save_to_cache=True)
+model = Model.load(path)
 outputs = model.predict(text, output_attentions=True, output_hidden_states=True)
 print(outputs)
