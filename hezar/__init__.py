b0803d2cc0da70c51bfbf8af46e344aa2b1497a0
arxyzan <arxyzan@gmail.com>
2023-03-19 15:25:43 +0330

    reorder imports in the main init file

--- hezar/__init__.py
+++ hezar/__init__.py
@@ -1,10 +1,11 @@
-__version__ = "0.3.0"
+__version__ = "0.4.3"
 
-from .configs import *
-from .data import *
-from .preprocessors import *
-from .models import *
-from .trainers import *
-from .utils import *
 from .registry import *
 from .builders import *
+from .configs import *
+from .data import *
+from .models import *
+from .pipelines import *
+from .preprocessors import *
+from .trainers import *
+from .utils import *
