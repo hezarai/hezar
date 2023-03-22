ad3dd7f15de298592d87e9e4844f89739cca66c0
arxyzan <arxyzan@gmail.com>
2023-03-22 17:09:37 +0330

    text_classification_example: minor

--- examples/text_classification_example.py
+++ examples/text_classification_example.py
@@ -1,9 +1,10 @@
 from hezar import build_model, Model, Tokenizer
 
-path = "hezar-ai/bert-fa-sentiment-digikala-snappfood"
-model = Model.load(path)
-# model = build_model('distilbert_text_classification', id2label={0: 'negative', 1: 'positive'})
-tokenizer = Tokenizer.load(path)
-inputs = tokenizer(["یه تست خیلی خفن"], return_tensors="pt")
-outputs = model.predict(inputs)
-print(outputs)
+hub_path = "hezar-ai/bert-fa-sentiment-digikala-snappfood"
+device = "cpu"
+model = Model.load(hub_path).to(device)
+tokenizer = Tokenizer.load(hub_path)
+inputs = ["کتابخانه هزار، بهترین کتابخانه هوش مصنوعیه"]
+model_inputs = tokenizer(inputs, return_tensors="pt", device=device)
+model_outputs = model.predict(model_inputs)
+print(model_outputs)
