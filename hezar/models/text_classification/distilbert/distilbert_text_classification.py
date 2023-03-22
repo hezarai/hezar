21778e669023c07827d937844f3736cac56a49f0
arxyzan <arxyzan@gmail.com>
2023-03-22 13:00:41 +0330

    distilbert_text_classification: refactor class

--- hezar/models/text_classification/distilbert/distilbert_text_classification.py
+++ hezar/models/text_classification/distilbert/distilbert_text_classification.py
@@ -3,7 +3,8 @@ A DistilBERT model for text classification built using HuggingFace Transformers
 """
 from typing import Dict
 
-from transformers import DistilBertConfig, DistilBertForSequenceClassification
+from torch import nn
+from transformers import DistilBertConfig, DistilBertModel
 
 from ....models import Model
 from ....registry import register_model
@@ -21,27 +22,37 @@ class DistilBertTextClassification(Model):
 
     def __init__(self, config: DistilBertTextClassificationConfig, **kwargs):
         super().__init__(config, **kwargs)
-        self.model = self._build()
+        self.distilbert = DistilBertModel(self._build_inner_config())
+        self.pre_classifier = nn.Linear(self.config.dim, self.config.dim)
+        self.classifier = nn.Linear(self.config.dim, self.config.num_labels)
+        self.dropout = nn.Dropout(self.config.seq_classif_dropout)
 
-    def _build(self):
+    def _build_inner_config(self):
         if self.config.num_labels is None and self.config.id2label is None:
             raise ValueError("Both `num_labels` and `id2label` are None. Please provide at least one of them!")
         if self.config.id2label and self.config.num_labels is None:
             self.config.num_labels = len(self.config.id2label)
-        config = DistilBertConfig(**self.config)
-        model = DistilBertForSequenceClassification(config)
-        return model
+        bert_config = DistilBertConfig(**self.config)
+        return bert_config
 
     def forward(self, inputs, **kwargs) -> Dict:
         input_ids = inputs.get("token_ids")
-        attention_mask = inputs.get("attention_mask", None)
-        labels = inputs.get("labels", None)
-        outputs = self.model(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            labels=labels,
-            **kwargs,
-        )
+        labels = inputs.get("labels")
+
+        lm_outputs = self.distilbert(input_ids=input_ids, **kwargs)
+        hidden_state = lm_outputs[0]
+        pooled_output = hidden_state[:, 0]
+        pooled_output = self.pre_classifier(pooled_output)
+        pooled_output = nn.ReLU()(pooled_output)
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        loss = nn.BCEWithLogitsLoss()(logits, labels) if labels else None
+        outputs = {
+            "loss": loss,
+            "logits": logits,
+            "hidden_states": lm_outputs.hidden_states,
+            "attentions": lm_outputs.attentions,
+        }
         return outputs
 
     def post_process(self, inputs, **kwargs) -> Dict:
