ec6da29976db8193bda333ce0fecd451718ec05f
arxyzan <arxyzan@gmail.com>
2023-03-28 08:26:17 +0330

    Trainer: change set_seed to static method

--- hezar/trainers/trainer.py
+++ hezar/trainers/trainer.py
@@ -77,7 +77,8 @@ class Trainer:
         self.metrics_manager = self._setup_metrics_manager(self.config.metrics)
         self.tensorboard = SummaryWriter()
 
-    def set_seed(self, seed):
+    @staticmethod
+    def set_seed(seed):
         torch.manual_seed(seed)
         np.random.seed(seed)
         random.seed(seed)
