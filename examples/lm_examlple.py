af3a70eb43dd39810cc5bedd1428e6fffbb7ff15
arxyzan <arxyzan@gmail.com>
2023-03-30 17:17:20 +0330

    lm_example: change to roberta

--- examples/lm_examlple.py
+++ examples/lm_examlple.py
@@ -1,11 +1,11 @@
 from hezar import build_model, Model, Tokenizer
 
 
-path = "hezarai/bert-base-fa"
+path = "hezarai/roberta-base-fa"
 tokenizer = Tokenizer.load(path)
 text = tokenizer(["hello guys"], return_tensors="pt")
 
-model = build_model("bert_lm")
+model = build_model("roberta_lm")
 outputs = model.predict(text, output_attentions=True, output_hidden_states=True)
 print(outputs)
 
