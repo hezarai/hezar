775f4858c4f3e17828f0dbf28f2c1635c63e66fc
arxyzan <arxyzan@gmail.com>
2023-04-02 13:36:37 +0330

    train_example: change model to roberta

--- examples/train_example.py
+++ examples/train_example.py
@@ -7,13 +7,13 @@ from hezar import (
     build_scheduler,
 )
 
-name = "bert_text_classification"
+name = "roberta_text_classification"
 
 dataset_config = {
     "path": "hezarai/sentiment_digikala_snappfood",
     "text_field": "text",
     "label_field": "label",
-    "tokenizer_path": "hezarai/bert-base-fa",
+    "tokenizer_path": "hezarai/roberta-base-fa",
 }
 train_dataset = build_dataset(name="text_classification", split="train", **dataset_config)
 eval_dataset = build_dataset(name="text_classification", split="test", **dataset_config)
@@ -25,7 +25,7 @@ lr_scheduler = build_scheduler("reduce_on_plateau", optimizer=optimizer)
 train_config = TrainConfig(
     name=name,
     device="cuda",
-    init_weights_from="hezarai/bert-base-fa",
+    init_weights_from="hezarai/roberta-base-fa",
     batch_size=8,
     num_train_epochs=5,
     checkpoints_dir="checkpoints/",
