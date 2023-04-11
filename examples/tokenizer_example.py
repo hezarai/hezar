74612c2ed6fb331c2e505233300657dcbafb8cbb
arxyzan <arxyzan@gmail.com>
2023-04-11 17:25:59 +0330

    tokenizer_example: minor

--- examples/tokenizer_example.py
+++ examples/tokenizer_example.py
@@ -1,7 +1,7 @@
 from hezar import Tokenizer
 
 
-tokenizer = Tokenizer.load(hub_or_local_path="hezarai/roberta-fa")
+tokenizer = Tokenizer.load(hub_or_local_path="hezarai/roberta-base-fa")
 encoded = tokenizer(["Hello guys", "my name is", "Aryan Shekarlaban"], return_attention_mask=True)
 decoded = tokenizer.decode(encoded["token_ids"])
 print(encoded)
