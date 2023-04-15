2f5d54c9cb33792578f95715bb65a802842017ac
arxyzan <arxyzan@gmail.com>
2023-04-15 13:26:55 +0330

    models: update init

--- hezar/models/__init__.py
+++ hezar/models/__init__.py
@@ -3,11 +3,9 @@ from .audio_classification import *
 from .image2text import *
 from .language_modeling import *
 from .miscellaneous import *
-from .ocr import *
 from .question_answering import *
 from .sequence_labeling import *
 from .speech_recognition import *
 from .text_classification import *
 from .text_detection import *
 from .text_summarization import *
-from .text_translation import *
