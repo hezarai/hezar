4703035fb783e843c15aef3449a0cbbb4e718c60
arxyzan <arxyzan@gmail.com>
2023-04-15 13:27:18 +0330

    remove pipelines from init

--- hezar/__init__.py
+++ hezar/__init__.py
@@ -5,7 +5,6 @@ from .builders import *
 from .configs import *
 from .data import *
 from .models import *
-from .pipelines import *
 from .preprocessors import *
 from .trainers import *
 from .utils import *
