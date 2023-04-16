bdc5522fc09eda5f7d9b5c37a8b40e3703038c04
arxyzan <arxyzan@gmail.com>
2023-04-16 10:31:12 +0430

    examples: minor

--- examples/text_classification_example.py
+++ examples/text_classification_example.py
@@ -1,10 +1,9 @@
 from hezar import build_model, Model, Tokenizer
 
-hub_path = "hezarai/bert-fa-sentiment-digikala-snappfood"
-device = "cpu"
-model = Model.load(hub_path).to(device)
+hub_path = "hezarai/roberta-fa-sentiment-digikala-snappfood"
+model = Model.load(hub_path, device="cpu")
 tokenizer = Tokenizer.load(hub_path)
 inputs = ["کتابخانه هزار، بهترین کتابخانه هوش مصنوعیه"]
-model_inputs = tokenizer(inputs, return_tensors="pt", device=device)
+model_inputs = tokenizer(inputs, return_tensors="pt", device="cpu")
 model_outputs = model.predict(model_inputs)
 print(model_outputs)
