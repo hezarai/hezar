a1695470fc4acad53c4c9a01e2515c0c5f9b475e
arxyzan <arxyzan@gmail.com>
2023-04-17 12:51:48 +0330

    tokenizer_example: hotfix

--- examples/tokenizer_example.py
+++ examples/tokenizer_example.py
@@ -6,7 +6,7 @@ encoded = tokenizer(["Hello guys", "my name is", "Aryan Shekarlaban"], return_at
 decoded = tokenizer.decode(encoded["token_ids"])
 print(encoded)
 print(decoded)
-tokenizer = Tokenizer.load(hub_or_local_path="hezarai/distilbert-fa")
+tokenizer = Tokenizer.load(hub_or_local_path="hezarai/distilbert-base-fa")
 encoded = tokenizer(["Hello guys", "my name is", "Aryan Shekarlaban"], return_attention_mask=True)
 decoded = tokenizer.decode(encoded["token_ids"])
 print(encoded)
