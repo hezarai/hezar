d32ed5cc2fe2c18f02a108c83dd89186089b836c
arxyzan <arxyzan@gmail.com>
2023-04-19 16:49:47 +0330

    version update: 0.11.1

--- hezar/__init__.py
+++ hezar/__init__.py
@@ -1,5 +1,3 @@
-__version__ = "0.11.0"
-
 from .registry import *
 from .builders import *
 from .configs import *
@@ -8,3 +6,6 @@ from .models import *
 from .preprocessors import *
 from .trainers import *
 from .utils import *
+
+
+__version__ = "0.11.1"
