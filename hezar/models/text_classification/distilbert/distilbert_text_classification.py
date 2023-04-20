distilbert_text_classification: fix loss calculation bug

hezar/models/text_classification/distilbert/distilbert_text_classification.py
@@ -46,7 +46,7 @@ class DistilBertTextClassification(Model):
         pooled_output = nn.ReLU()(pooled_output)
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
-        loss = nn.BCEWithLogitsLoss()(logits, labels) if labels else None
+        loss = nn.CrossEntropyLoss()(logits, labels) if labels is not None else None
         outputs = {
             "loss": loss,
             "logits": logits,