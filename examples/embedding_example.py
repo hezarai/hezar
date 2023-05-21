9f94d95b4a4ea0026a2a3f519176712b5229a4a3
arxyzan <arxyzan@gmail.com>
2023-05-21 13:32:02 +0330

    Add `embedding_example.py` to examples

--- examples/embedding_example.py
+++ examples/embedding_example.py
@@ -1,6 +1,8 @@
 from hezar import build_embedding
+from hezar import Embedding
 
-e = build_embedding("word2vec_cbow")
-
-e.push_to_hub("arxyzan/embedding_test", private=True)
-...
+emb = Embedding.load("arxyzan/embedding_test")
+word2vec = build_embedding("word2vec")
+corpus = "This is our test corpus to see if this works".split(" ")
+word2vec.train(corpus)
+word2vec.push_to_hub("arxyzan/embedding_test", private=True)
