1902ec5ecc1f95ddb985f27a22b4ab622b2bfa57
arxyzan <arxyzan@gmail.com>
2023-05-21 20:10:29 +0330

    Apply minor changes in embedding_example.py

--- examples/embedding_example.py
+++ examples/embedding_example.py
@@ -1,8 +1,8 @@
 from hezar import build_embedding
 from hezar import Embedding
 
-emb = Embedding.load("arxyzan/embedding_test")
 word2vec = build_embedding("word2vec")
 corpus = "This is our test corpus to see if this works".split(" ")
+corpus = [[x] for x in corpus]
 word2vec.train(corpus)
 word2vec.push_to_hub("arxyzan/embedding_test", private=True)
