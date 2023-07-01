b1356c42c25893fce34e1c3ad06f3ea72ca065c3
arxyzan <arxyzan@gmail.com>
2023-07-01 13:01:58 +0330

    Add `load` to `Dataset`

---
+++ examples/dataset_example.py
@@ -0,0 +1,9 @@
+from torch.utils.data import DataLoader
+
+from hezar.data.datasets import Dataset
+
+dataset = Dataset.load("hezarai/lscp-500k", tokenizer_path="hezarai/distilbert-base-fa")
+
+loader = DataLoader(dataset, batch_size=16, shuffle=True, collate_fn=dataset.data_collator)
+itr = iter(loader)
+print(next(itr))
--- hezar/data/datasets/dataset.py
+++ hezar/data/datasets/dataset.py
@@ -1,15 +1,57 @@
+import os
+from typing import Union, Optional
+
 from torch.utils.data import Dataset as TorchDataset
 
 from ...configs import DatasetConfig
+from ...constants import DEFAULT_DATASET_CONFIG_FILE
+from ...utils import get_module_class
 
 
 class Dataset(TorchDataset):
+    """
+    Base class for all datasets in Hezar.
+
+    Args:
+        config:
+        **kwargs:
+    """
+    config_filename = DEFAULT_DATASET_CONFIG_FILE
+
     def __init__(self, config: DatasetConfig, **kwargs):
         self.config = config.update(kwargs)
         self.preprocessor = None
 
     def __len__(self):
-        ...
+        raise NotImplementedError
 
     def __getitem__(self, index):
-        ...
+        raise NotImplementedError
+
+    @classmethod
+    def load(
+        cls,
+        hub_path: Union[str, os.PathLike],
+        config_filename: Optional[str] = None,
+        split: Optional[str] = None,
+        **kwargs,
+    ):
+        """
+        Load the dataset from hub.
+
+        Args:
+            hub_path: Path to dataset from hub or locally
+            config_filename: Dataset config file name
+            split: Dataset split, defaults to "train"
+            **kwargs: Config parameters as keyword arguments
+
+        Returns:
+
+        """
+        split = split or "train"
+        config_filename = config_filename or cls.config_filename
+        dataset_config = DatasetConfig.load(hub_path, filename=config_filename, repo_type="dataset", **kwargs)
+        dataset_class = get_module_class(dataset_config.name, module_type="dataset")
+        dataset_config.path = hub_path
+        dataset = dataset_class(dataset_config, split=split)
+        return dataset
