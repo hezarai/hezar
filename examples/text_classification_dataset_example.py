b8cddadcf78485e86b914ba7284ce070674c7063
arxyzan <arxyzan@gmail.com>
2023-07-01 12:45:05 +0330

    Fix names mismatch in text_classification_dataset_example.py

--- examples/text_classification_dataset_example.py
+++ examples/text_classification_dataset_example.py
@@ -1,17 +1,17 @@
 from torch.utils.data import DataLoader
 
-from hezar.data.datasets import SequenceLabelingDataset, SequenceLabelingDatasetConfig
+from hezar.data.datasets import TextClassificationDataset, TextClassificationDatasetConfig
 
 
-config = SequenceLabelingDatasetConfig(
-    name="sequence_labeling",
-    path="hezarai/lscp-500k",
-    tokens_field="tokens",
-    tags_field="pos_tags",
+config = TextClassificationDatasetConfig(
+    name="text_classfication",
+    path="hezarai/sentiment_digikala_snappfood",
+    text_field="text",
+    label_field="label",
     tokenizer_path="hezarai/distilbert-base-fa",
 )
 
-dataset = SequenceLabelingDataset(config, split="train")
+dataset = TextClassificationDataset(config, split="train")
 print(dataset[0])
 
 loader = DataLoader(dataset, batch_size=16, shuffle=True, collate_fn=dataset.data_collator)
