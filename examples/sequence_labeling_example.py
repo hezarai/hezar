8893adfd20a3cf7d439b1a2309f49e63ff8f763d
arxyzan <arxyzan@gmail.com>
2023-07-02 11:59:08 +0330

    Add BERT for sequence labeling example

---
+++ examples/sequence_labeling_example.py
@@ -0,0 +1,10 @@
+from hezar import build_model, Tokenizer
+
+hub_path = "hezarai/bert-base-fa"
+tokenizer = Tokenizer.load(hub_path)
+# dummy model just to test
+model = build_model("bert_sequence_labeling", id2label={0: "NOUN", 1: "VERB", 2: "ADJ", 3: "ADV", 4: "DET"})
+inputs = ["سلام بر خدای آمون"]
+model_inputs = tokenizer(inputs, return_word_ids=True, return_tokens=True, padding=True, truncation=True, return_tensors="pt")
+x = model.predict(model_inputs)
+print(x)
