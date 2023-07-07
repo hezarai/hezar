bb2c35b8762644d34eae4e03bf719cbd26455cb2
arxyzan <arxyzan@gmail.com>
2023-07-08 00:32:39 +0330

    Load dataset from Hub in `train_example.py`

--- examples/train_example.py
+++ examples/train_example.py
@@ -1,20 +1,16 @@
 from hezar import (
     TrainConfig,
     Trainer,
-    build_dataset,
+    Dataset,
     build_model,
 )
 
 name = "distilbert_text_classification"
+dataset_path = "hezarai/sentiment_digikala_snappfood"
+lm_path = "hezarai/distilbert-base-fa"
 
-dataset_config = {
-    "path": "hezarai/sentiment_digikala_snappfood",
-    "text_field": "text",
-    "label_field": "label",
-    "tokenizer_path": "hezarai/distilbert-base-fa",
-}
-train_dataset = build_dataset(name="text_classification", split="train", **dataset_config)
-eval_dataset = build_dataset(name="text_classification", split="test", **dataset_config)
+train_dataset = Dataset.load(dataset_path, split="train", tokenizer_path=lm_path)
+eval_dataset = Dataset.load(dataset_path, split="test", tokenizer_path=lm_path)
 
 model = build_model(name, id2label=train_dataset.id2label)
 
@@ -22,7 +18,7 @@ train_config = TrainConfig(
     name=name,
     device="cuda",
     optimizer={"name": "adam", "lr": 2e-5, "scheduler": {"name": "reduce_on_plateau"}},
-    init_weights_from="hezarai/distilbert-base-fa",
+    init_weights_from=lm_path,
     batch_size=8,
     num_epochs=5,
     checkpoints_dir="checkpoints/",
