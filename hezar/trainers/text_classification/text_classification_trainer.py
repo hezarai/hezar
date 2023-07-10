3b1520588a68aa4e3189cd3c5258bdab60de6ed3
arxyzan <arxyzan@gmail.com>
2023-07-10 14:23:16 +0330

    Apply new design changes to TextClassificationTrainer

--- hezar/trainers/text_classification/text_classification_trainer.py
+++ hezar/trainers/text_classification/text_classification_trainer.py
@@ -1,10 +1,9 @@
-from typing import Callable, Dict, Iterable, Union
+from typing import Callable, Dict, Iterable, Union, Tuple
 
 import torch
 
 from hezar.constants import MetricType
 
-from ... import Metric, snake_case
 from ...builders import build_metric
 from ...configs import TrainerConfig
 from ...constants import (
@@ -14,7 +13,8 @@ from ...constants import (
 )
 from ...data.datasets import Dataset
 from ...models import Model
-from ...utils import get_logger
+from ...metrics import Metric
+from ...utils import get_logger, snake_case
 from ..trainer import Trainer
 from ..trainer_utils import MetricsManager
 
@@ -23,7 +23,7 @@ logger = get_logger(__name__)
 
 class TextClassificationTrainer(Trainer):
     """
-    A general but fully featured model trainer/evaluator for Hezar models.
+    A trainer for all text classification models
 
     Args:
         model ([`Model`] or `torch.nn.Module`): The main model to train and evaluate
@@ -58,17 +58,9 @@ class TextClassificationTrainer(Trainer):
             data_collator=data_collator,
             optimizer=optimizer,
             lr_scheduler=lr_scheduler)
+        self.criterion = torch.nn.CrossEntropyLoss()
 
     def _setup_metrics_manager(self, metrics: Iterable[Union[str, Callable, Metric, MetricType]]) -> MetricsManager:
-        """
-        Set up metrics manager to track and update metrics like loss, accuracy, f1, etc.
-
-        Args:
-            metrics: A dict of metrics names and their kwargs {metric_name: **kwargs}
-
-        Returns:
-             A MetricsManager instance
-        """
         metrics_dict = {"loss": None}
         for metric in metrics:
             if isinstance(metric, str):
@@ -85,53 +77,6 @@ class TextClassificationTrainer(Trainer):
         metrics_manager = MetricsManager(metrics_dict)
         return metrics_manager
 
-    def training_step(self, input_batch: Dict[str, torch.Tensor]):
-        """
-        Train one batch of data and return metrics outputs
-
-        Args:
-            input_batch: A batch of inputs to train
-
-        Returns:
-            The metrics results
-        """
-        input_batch = {k: v.to(self.device) for k, v in input_batch.items() if isinstance(v, torch.Tensor)}
-
-        with torch.autocast(device_type=self.device_type, dtype=self.autocast_dtype, enabled=self.config.use_amp):
-            outputs = self.model(input_batch)
-            if "loss" not in outputs:
-                raise ValueError("Model outputs must contain `loss`!")
-            loss: torch.Tensor = outputs["loss"]
-
-        self.scaler.scale(loss).backward()
-        self.scaler.step(self.optimizer)
-        self.scaler.update()
-        self.optimizer.zero_grad()
-
-        results = self.metrics_manager.compute(outputs["logits"].detach().cpu(), input_batch["labels"].detach().cpu())
-        results["loss"] = loss.item()
-
-        return results
-
-    def evaluation_step(self, input_batch: Dict[str, torch.Tensor]):
-        """
-        Evaluate one batch of data and return metrics outputs
-
-        Args:
-            input_batch: A batch of inputs to train
-
-        Returns:
-            The metrics results
-        """
-        input_batch = {k: v.to(self.device) for k, v in input_batch.items() if isinstance(v, torch.Tensor)}
-
-        with torch.autocast(device_type=self.device_type, dtype=self.autocast_dtype, enabled=self.config.use_amp):
-            outputs = self.model(input_batch)
-            if "loss" not in outputs:
-                raise ValueError("Model outputs must contain `loss`!")
-            loss: torch.Tensor = outputs["loss"]
-
-        results = self.metrics_manager.compute(outputs["logits"].detach().cpu(), input_batch["labels"].detach().cpu())
-        results["loss"] = loss.item()
-
-        return results
+    def compute_loss(self, logits, labels, **kwargs) -> torch.Tensor:
+        loss = self.criterion(logits, labels)
+        return loss
