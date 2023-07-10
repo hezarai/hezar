fbfd847529d9a84c8c179ef6945b9fccda340550
arxyzan <arxyzan@gmail.com>
2023-07-10 14:21:51 +0330

    Add common_utils.py

---
+++ hezar/utils/common_utils.py
@@ -0,0 +1,28 @@
+from typing import Dict, List
+from re import sub
+
+__all__ = [
+    "snake_case",
+    "permute_dict_list",
+]
+
+
+def snake_case(s):
+    return '_'.join(
+        sub('([A-Z][a-z]+)', r' \1',
+            sub('([A-Z]+)', r' \1',
+                s.replace('-', ' '))).split()).lower()
+
+
+def permute_dict_list(dict_list: List[Dict]) -> Dict[str, List]:
+    """
+    Convert a list of dictionaries to a dictionary of lists
+
+    Args:
+        dict_list: Input list of dicts
+
+    Returns:
+
+    """
+    d = {key: [x[key] for x in dict_list] for key in dict_list[0]}
+    return d
