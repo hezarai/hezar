48ab07ffb985129202b376eecf70e7ef3a2e5c5c
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2023-07-14 11:30:17 +0330

    Remove name from F1Config

--- hezar/metrics/f1.py
+++ hezar/metrics/f1.py
@@ -1,17 +1,15 @@
-from typing import Any, Dict, Iterable
 from dataclasses import dataclass
+from typing import Any, Dict, Iterable
 
 from sklearn.metrics import f1_score
 
-from ..registry import register_metric
-from ..configs import MetricConfig
-from ..constants import MetricType
 from .metric import Metric
+from ..configs import MetricConfig
+from ..registry import register_metric
 
 
 @dataclass
 class F1Config(MetricConfig):
-    name: str = MetricType.F1
     pos_label: int = None
     average: str = None
     sample_weight: Iterable[float] = None
@@ -23,15 +21,14 @@ class F1(Metric):
         super().__init__(config, **kwargs)
 
     def compute(
-        self,
-        predictions=None,
-        targets=None,
-        labels=None,
-        pos_label=1,
-        average="binary",
-        sample_weight=None
+            self,
+            predictions=None,
+            targets=None,
+            labels=None,
+            pos_label=1,
+            average="binary",
+            sample_weight=None
     ) -> Dict[str, Any]:
-
         pos_label = pos_label or self.config.pos_label
         average = average or self.config.average
         sample_weight = sample_weight or self.config.sample_weight
