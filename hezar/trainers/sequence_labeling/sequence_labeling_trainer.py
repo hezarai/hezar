1a3386e5dbee5a559e1a19e63e280cb9c4c80115
arxyzan <arxyzan@gmail.com>
2023-07-14 17:47:49 +0330

    Cleanup sequence_labeling_trainer.py

--- hezar/trainers/sequence_labeling/sequence_labeling_trainer.py
+++ hezar/trainers/sequence_labeling/sequence_labeling_trainer.py
@@ -1,22 +1,10 @@
-from typing import Callable, Dict, Iterable, Union, Tuple
-
 import torch
 
-from hezar.constants import MetricType
-
-from ...builders import build_metric
 from ...configs import TrainerConfig
-from ...constants import (
-    DEFAULT_DATASET_CONFIG_FILE,
-    DEFAULT_TRAINER_CONFIG_FILE,
-    DEFAULT_TRAINER_SUBFOLDER,
-)
 from ...data.datasets import Dataset
 from ...models import Model
-from ...metrics import Metric
-from ...utils import get_logger, snake_case
+from ...utils import get_logger
 from ..trainer import Trainer
-from ..trainer_utils import MetricsManager
 
 logger = get_logger(__name__)
 
@@ -35,10 +23,7 @@ class SequenceLabelingTrainer(Trainer):
         lr_scheduler: Optional learning-rate scheduler
 
     """
-
-    trainer_subfolder = DEFAULT_TRAINER_SUBFOLDER
-    trainer_config_file = DEFAULT_TRAINER_CONFIG_FILE
-    dataset_config_file = DEFAULT_DATASET_CONFIG_FILE
+    AVAILABLE_METRICS = []
 
     def __init__(
         self,
@@ -61,23 +46,9 @@ class SequenceLabelingTrainer(Trainer):
         )
         self.criterion = torch.nn.CrossEntropyLoss()
 
-    def _setup_metrics_manager(self, metrics: Iterable[Union[str, Callable, Metric, MetricType]]) -> MetricsManager:
-        metrics_dict = {"loss": None}
-        for metric in metrics:
-            if isinstance(metric, str):
-                metrics_dict[metric] = build_metric(metric)
-            elif isinstance(metric, Metric):
-                metrics_dict[metric] = metric.compute
-            elif callable(metric):
-                if hasattr(metric, "compute"):
-                    metrics_dict[snake_case(metric.__name__)] = metric.compute
-                else:
-                    metrics_dict[snake_case(metric.__name__)] = metric
-            else:
-                raise ValueError(f"Metric {metric} is not a valid metric!")
-        metrics_manager = MetricsManager(metrics_dict)
-        return metrics_manager
-
     def compute_loss(self, logits, labels, **kwargs) -> torch.Tensor:
         loss = self.criterion(logits.view(-1, self.model.config.num_labels), labels.view(-1))
         return loss
+
+    def compute_metrics(self, predictions, labels, **kwargs):
+        raise NotImplementedError
