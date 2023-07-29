9f126e1a9c93ef9de178201ae7d8329191acbc0a
arxyzan <arxyzan@gmail.com>
2023-07-29 17:02:06 +0330

    Minor changes in examples

--- examples/sequence_labeling_example.py
+++ examples/sequence_labeling_example.py
@@ -3,6 +3,5 @@ from hezar import Model
 hub_path = "hezarai/bert-fa-pos-lscp-500k"
 model = Model.load(hub_path)
 inputs = ["سلام بر فارسی زبانان شریف"]
-x = model.predict(inputs)
-model.push_to_hub("hezarai/bert-fa-pos-lscp-500k")
-print(x)
+outputs = model.predict(inputs)
+print(outputs)
