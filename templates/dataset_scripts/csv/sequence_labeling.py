52b200aa58691a0c4db7bd5a93fdab8196a3c3a0
arxyzan <arxyzan@gmail.com>
2023-07-31 15:49:34 +0330

    Remove extra comments in templates

--- templates/dataset_scripts/csv/sequence_labeling.py
+++ templates/dataset_scripts/csv/sequence_labeling.py
@@ -1,8 +1,3 @@
-# TODO
-"""
-PROVIDE GENERAL DESCRIPTION OF ANYTHING REGARDING THIS LOADING SCRIPT OF THE DATASET. (OPTIONAL)
-"""
-
 import csv
 
 import datasets
@@ -10,74 +5,40 @@ import datasets
 
 logger = datasets.logging.get_logger(__name__)
 
-# TODO
-_CITATION = """\
-ORIGINAL CITATION HERE. (OPTIONAL BUT RECOMMENDED)
-"""
+_CITATION = """Citation"""
 
-# TODO
-_DESCRIPTION = """\
-THIS IS THE MAIN DESCRIPTION. IF YOUR DATASET IS FROM A PAPER, IT'S RECOMMENDED TO PROVIDE THE ABSTRACT OF THE PAPER.
-THIS VALUE WILL BE USED AS THE DATASET'S MAIN DESCRIPTION. TRY TO BE SHORT BUT EXPLICIT HERE. (OPTIONAL BUT RECOMMENDED)
-"""
+_DESCRIPTION = """Description"""
 
-# TODO
-"""
-PUT THE URL TO THE CSV FILES HERE. USUALLY WE UPLOAD THE FILES TO THE HUB ON THE SAME REPOSITORY THAT THIS SCRIPT LIVES.
-FOR EXAMPLE:
-
-```python
-_DOWNLOAD_URLS = {
-    "train": "https://huggingface.co/datasets/hezarai/lscp-500k/resolve/main/lscp-500k_train.csv",
-    "test": "https://huggingface.co/datasets/hezarai/lscp-500k/resolve/main/lscp-500k_test.csv",
-}
-```
-"""
 _DOWNLOAD_URLS = {
     "train": "PUT PATH TO TRAIN CSV FILE HERE (REQUIRED)",
     "dev": "PUT PATH TO DEV CSV FILE HERE (OPTIONAL)",
     "test": "PUT PATH TO TEST CSV FILE HERE (OPTIONAL BUT RECOMMENDED)",
 }
 
-# TODO
-"""
-YOU HAVE TO PROVIDE A BUILDER CONFIG CLASS LIKE BELOW FOR UNDER YOUR DATASET'S NAME
-"""
 
-
-# TODO
 class DatasetNameConfig(datasets.BuilderConfig):
     def __init__(self, **kwargs):
         super(DatasetNameConfig, self).__init__(**kwargs)
 
 
-# TODO
 class DatasetName(datasets.GeneratorBasedBuilder):
-    """OPTIONAL DOCSTRING FOR THIS CLASS"""
-
     BUILDER_CONFIGS = [
         DatasetNameConfig(
-            name="NAME FOR YOUR DATASET",
-            version=datasets.Version("DATASET VERSION"),
-            description=_DESCRIPTION,  # PUT YOUR OWN DESCRIPTION IF THE ABOVE `_DESCRIPTION` IS NOT PROVIDED
+            name="",
+            version=datasets.Version(""),
+            description=_DESCRIPTION,
         ),
     ]
 
-    # TODO
     def _info(self):
-        """
-        FOR SEQUENCE LABELING FOLLOW THE BELOW STRUCTURE.
-        """
         return datasets.DatasetInfo(
             description=_DESCRIPTION,
-            # TODO
-            # THESE FEATURES ARE THE COLUMN NAMES FOR YOUR DATASET. FOR SEQUENCE LABELING FOLLOW THIS STRUCTURE
             features=datasets.Features(
                 {
                     "tokens": datasets.Sequence(datasets.Value("string")),
                     # TODO YOU SHOULD PUT THE EXTRACTED UNIQUE TAGS IN YOUR DATASET HERE. THIS LIST IS JUST AN EXAMPLE
                     """
-                    TO EXTRACT UNIQUE TAGS FROM A PANDAS DATAFRAME USE THIS CODE AND PASTE THE OUTPUT LIST BELOW.
+                    To extract unique tags from a pandas dataframe use this code and paste the output list below.
 
                     ```python
                     unique_tags = df["TAGS_COLUMN_NAME"].explode().unique()
@@ -95,13 +56,11 @@ class DatasetName(datasets.GeneratorBasedBuilder):
             citation=_CITATION,
         )
 
-    # TODO USUALLY YOU WON'T NEED TO CHANGE THIS METHOD. THIS JUST RETURNS A GENERATOR FOR THE EXTRACTED FILES
     def _split_generators(self, dl_manager):
         """
-        Returns SplitGenerators.
+        Return SplitGenerators.
         """
 
-        # THIS DOWNLOADS AND EXTRACTS THE CSV FILES TO CACHE PATH
         train_path = dl_manager.download_and_extract(_DOWNLOAD_URLS["train"])
         test_path = dl_manager.download_and_extract(_DOWNLOAD_URLS["test"])
 
@@ -117,9 +76,9 @@ class DatasetName(datasets.GeneratorBasedBuilder):
     # TODO
     def _generate_examples(self, filepath):
         """
-        PER EACH file_path READ THE CSV FILE AND ITERATE IT.
-        FOR EACH ROW YIELD A TUPLE OF (ID, {"tokens": ..., "tags": ..., ...})
-        EACH CALL TO THIS METHOD YIELDS AN OUTPUT LIKE BELOW:
+        Per each file_path read the csv file and iterate it.
+        For each row yield a tuple of (id, {"tokens": ..., "tags": ..., ...})
+        Each call to this method yields an output like below:
         ```
         (124, {"tokens": ["hello", "world"], "pos_tags": ["NOUN", "NOUN"]})
         ```
@@ -130,10 +89,10 @@ class DatasetName(datasets.GeneratorBasedBuilder):
                 csv_file, quotechar='"', skipinitialspace=True
             )
 
-            # UNCOMMENT BELOW LINE TO SKIP THE FIRST ROW IF YOUR CSV FILE HAS A HEADER ROW
+            # Uncomment below line to skip the first row if your csv file has a header row
             # next(csv_reader, None)
 
             for id_, row in enumerate(csv_reader):
                 tokens, pos_tags = row
-                # YOUR OWN PREPROCESSING HERE (OPTIONAL)
+                # Optional preprocessing here
                 yield id_, {"tokens": tokens, "pos_tags": pos_tags}
