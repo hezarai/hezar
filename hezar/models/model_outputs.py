e48ced7dc32a10ec9afb53d383956e99a1aa1a94
arxyzan <arxyzan@gmail.com>
2023-08-05 11:35:21 +0330

    Add `model_outputs.py`

---
+++ hezar/models/model_outputs.py
@@ -0,0 +1,63 @@
+"""
+Define all model outputs here
+"""
+from dataclasses import dataclass, asdict
+from typing import List, Optional
+
+import torch
+
+
+@dataclass
+class ModelOutputs:
+    """
+    Base class for all model outputs (named based on tasks)
+    """
+
+    def dict(self):
+        return asdict(self)
+
+    def __getitem__(self, item):
+        try:
+            return self.dict()[item]
+        except KeyError:
+            raise AttributeError(f"`{self.__class__.__name__}` has no attribute `{item}`!")
+
+    def __len__(self):
+        return len(self.dict())
+
+    def __iter__(self):
+        return iter(self.dict())
+
+    def keys(self):
+        return list(self.dict().keys())
+
+    def values(self):
+        return list(self.dict().values())
+
+    def items(self):
+        return self.dict().items()
+
+
+@dataclass
+class LanguageModelingOutputs(ModelOutputs):
+    last_hidden_state: Optional[torch.FloatTensor] = None
+    hidden_state: Optional[torch.FloatTensor] = None
+    attentions: Optional[torch.FloatTensor] = None
+
+
+@dataclass
+class TextClassificationOutputs(ModelOutputs):
+    labels: Optional[List[str]] = None
+    probs: Optional[List[float]] = None
+
+
+@dataclass
+class SequenceLabelingOutputs(ModelOutputs):
+    tokens: Optional[List[List[str]]] = None
+    tags: Optional[List[List[str]]] = None
+    probs: Optional[List[List[float]]] = None
+
+
+@dataclass
+class Text2TextOutputs(ModelOutputs):
+    generated_texts: Optional[List[str]] = None
