2fb8cf69c48cde50bfd3532afa7028f9b05bb617
arxyzan <arxyzan@gmail.com>
2023-08-07 01:30:02 +0330

    Inherit `Text2TextModel` in text2text models

--- hezar/models/text2text/t5/t5_text2text.py
+++ hezar/models/text2text/t5/t5_text2text.py
@@ -1,15 +1,14 @@
-from typing import Dict, List, Union
+from typing import Dict
 
-import torch
 from transformers import T5Config, T5ForConditionalGeneration
 
-from ....models import Model
 from ....registry import register_model
+from ..text2text import Text2TextModel
 from .t5_text2text_config import T5Text2TextConfig
 
 
 @register_model("t5_text2text", config_class=T5Text2TextConfig)
-class T5Text2Text(Model):
+class T5Text2Text(Text2TextModel):
     """
     T5 for text to text generation
     """
@@ -65,28 +64,3 @@ class T5Text2Text(Model):
             outputs = {"output_ids": output_ids}
 
         return outputs
-
-    def preprocess(self, inputs: Union[str, List[str]], **kwargs):
-        if isinstance(inputs, str):
-            inputs = [inputs]
-        if "text_normalizer" in self.preprocessor:
-            normalizer = self.preprocessor["text_normalizer"]
-            inputs = normalizer(inputs)
-        tokenizer = self.preprocessor[self.tokenizer_name]
-        inputs = tokenizer(inputs, return_tensors="pt", device=self.device)
-        return inputs
-
-    def post_process(self, inputs, **kwargs):
-        records = []
-        tokenizer = self.preprocessor["sentencepiece_unigram_tokenizer"]
-        for output_ids in inputs["output_ids"][0]:
-            if isinstance(output_ids, torch.Tensor):
-                output_ids = output_ids.numpy().tolist()
-            record = {
-                "output_text": tokenizer.decode(
-                    output_ids,
-                    skip_special_tokens=True,
-                )
-            }
-            records.append(record)
-        return records
