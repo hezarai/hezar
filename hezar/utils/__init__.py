ce9976a965142ae89565d8bbb8fc7f0752026ed8
arxyzan <arxyzan@gmail.com>
2023-08-06 13:36:12 +0330

    Add `file_utils.py`

--- hezar/utils/__init__.py
+++ hezar/utils/__init__.py
@@ -1,6 +1,7 @@
 from .common_utils import *
 from .core_utils import *
 from .context_managers import *
+from .file_utils import *
 from .logging import *
 from .hub_utils import *
 from .registry_utils import *
---
+++ hezar/utils/file_utils.py
@@ -0,0 +1,26 @@
+import gzip
+import shutil
+
+from .logging import get_logger
+
+logger = get_logger(__name__)
+
+__all__ = [
+    "gunzip"
+]
+
+
+def gunzip(src_path, dest_path):
+    """
+    Unzip a .gz file from `src_path` and extract to `dest_path`
+    Args:
+        src_path: Path to .gz file
+        dest_path: Path to the destination file
+
+    Returns:
+
+    """
+    with gzip.open(src_path, "rb") as f_in:
+        with open(dest_path, "wb") as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    logger.debug(f"Extracted {src_path} to {dest_path}")
