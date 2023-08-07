b663f621bade1313fd3fa7df8f99f85f37d30964
arxyzan <arxyzan@gmail.com>
2023-08-07 09:24:26 +0330

    Update embedding_example.py

--- examples/embedding_example.py
+++ examples/embedding_example.py
@@ -1,8 +1,5 @@
-from hezar import build_embedding
 from hezar import Embedding
 
-word2vec = build_embedding("word2vec")
-corpus = "This is our test corpus to see if this works".split(" ")
-corpus = [[x] for x in corpus]
-word2vec.train(corpus)
-word2vec.push_to_hub("arxyzan/embedding_test", private=True)
+fasttext = Embedding.load("hezarai/fasttext-fa-300")
+most_similar = fasttext.most_similar("هزار")
+print(most_similar)
