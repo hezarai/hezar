3faf9a26ec2eae9e0ba8a7d1b4dfa5b988284cc9
arxyzan <arxyzan@gmail.com>
2023-08-10 15:06:52 +0330

    Add `feature_extractors` to `preprocessors`

--- hezar/preprocessors/__init__.py
+++ hezar/preprocessors/__init__.py
@@ -1,3 +1,4 @@
 from .preprocessor import Preprocessor, PreprocessorsContainer
 from .text_normalizer import TextNormalizer, TextNormalizerConfig
+from .feature_extractors import *
 from .tokenizers import *
