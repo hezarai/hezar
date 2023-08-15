6229ced2669b640d79ebf91a7caa48c130381e70
arxyzan <arxyzan@gmail.com>
2023-08-16 00:07:32 +0330

    Add cast column to speech recognition example

--- examples/speech_recognition_example.py
+++ examples/speech_recognition_example.py
@@ -1,7 +1,8 @@
 from hezar import Model
-from datasets import load_dataset
+from datasets import load_dataset, Audio
 
 ds = load_dataset("mozilla-foundation/common_voice_11_0", "fa", split="test")
+df = ds.cast_column("audio", Audio(sampling_rate=16000))
 sample = ds[1001]
 whisper = Model.load("hezarai/whisper-small-fa")
 transcript = whisper.predict(sample["path"])  # or pass `sample["audio"]["array"]` (with the right sample rate)
