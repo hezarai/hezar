ac6ce8626bd1bbf4a95df579ce4adf40834e93d5
arxyzan <arxyzan@gmail.com>
2023-08-15 16:12:44 +0330

    Add librosa to integrations

--- hezar/integrations.py
+++ hezar/integrations.py
@@ -1,13 +1,15 @@
-import importlib
+import importlib.util
 
 
 __all__ = [
     "is_soundfile_available",
+    "is_librosa_available",
 ]
 
 
 def is_soundfile_available():
-    try:
-        importlib.import_module("soundfile")
-    except ImportError:
-        raise ImportError("soundfile is not installed!")
+    return importlib.util.find_spec("soundfile") is not None
+
+
+def is_librosa_available():
+    return importlib.util.find_spec("librosa") is not None
