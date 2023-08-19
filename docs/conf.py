4917b2b1cdd77e26f013e9447821f33b4078ea46
arxyzan <arxyzan@gmail.com>
2023-08-19 14:07:22 +0330

    Refactor docs

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
+    "myst_parser",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
@@ -25,7 +26,21 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
---
+++ docs/tutorial/index.rst
@@ -0,0 +1,11 @@
+Tutorial
+========
+.. toctree::
+   :maxdepth: 2
+
+   overview.md
+   installation.md
+   models.md
+   datasets.md
+   training.md
+   preprocessors.md
