85359c1d46ddd9aac8befcfcabc2549c12910f43
arxyzan <arxyzan@gmail.com>
2023-08-23 18:11:17 +0330

    Exclude docs/README.md in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md']
 
 myst_enable_extensions = [
     "amsmath",
