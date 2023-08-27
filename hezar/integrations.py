c4210d1741c4c95539f7c3025fa45f31205900a7
arxyzan <arxyzan@gmail.com>
2023-08-27 17:32:26 +0330

    Add other libs to integrations.py

--- hezar/integrations.py
+++ hezar/integrations.py
@@ -1,10 +1,26 @@
 import importlib.util
 
-
 __all__ = [
+    "is_transformers_available",
+    "is_datasets_available",
+    "is_tokenizers_available",
     "is_soundfile_available",
     "is_librosa_available",
+    "is_wandb_available",
+    "is_gensim_available",
 ]
+
+
+def is_transformers_available():
+    return importlib.util.find_spec("transformers") is not None
+
+
+def is_datasets_available():
+    return importlib.util.find_spec("datasets") is not None
+
+
+def is_tokenizers_available():
+    return importlib.util.find_spec("tokenizers") is not None
 
 
 def is_soundfile_available():
@@ -13,3 +29,11 @@ def is_soundfile_available():
 
 def is_librosa_available():
     return importlib.util.find_spec("librosa") is not None
+
+
+def is_wandb_available():
+    return importlib.util.find_spec("wandb") is not None
+
+
+def is_gensim_available():
+    return importlib.util.find_spec("gensim") is not None
