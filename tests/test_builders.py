bd791578230e822c18a1d3711b96102808100373
arxyzan <arxyzan@gmail.com>
2023-09-07 13:59:00 +0330

    Add test_builders.py

--- tests/test_builders.py
+++ tests/test_builders.py
@@ -1,26 +1,61 @@
-from hezar.builders import *
-
-
 def build_models():
-    from hezar.registry import models_registry
+    from hezar import list_available_models, build_model
 
-    for name, module in models_registry.items():
-        config = module["config_class"]()
-        if hasattr(config, "num_labels"):
-            model = build_model(name, config=module["config_class"](), num_labels=2)
-        else:
-            model = build_model(name, config=module["config_class"]())
-        print(f"Succesfully built `{name}`")
+    for name in list_available_models():
+        try:
+            build_model(name)
+        except Exception as e:
+            raise e
+        print(f"Successfully built `{name}`")
 
 
 def build_preprocessors():
-    from hezar.registry import preprocessors_registry
+    from hezar import list_available_preprocessors, build_preprocessor
 
-    for name, module in preprocessors_registry.items():
-        preprocessor = build_preprocessor(name, config=module["config_class"]())
-        print(f"Succesfully built `{name}`")
+    for name in list_available_preprocessors():
+        try:
+            build_preprocessor(name)
+        except Exception as e:
+            raise e
+        print(f"Successfully built `{name}`")
+
+
+def build_embeddings():
+    from hezar import list_available_embeddings, build_embedding
+
+    for name in list_available_embeddings():
+        try:
+            build_embedding(name)
+        except Exception as e:
+            raise e
+        print(f"Successfully built `{name}`")
+
+
+def build_datasets():
+    from hezar import list_available_datasets, build_dataset
+
+    for name in list_available_datasets():
+        try:
+            build_dataset(name)
+        except Exception as e:
+            raise e
+        print(f"Successfully built `{name}`")
+
+
+def build_metrics():
+    from hezar import list_available_metrics, build_metric
+
+    for name in list_available_metrics():
+        try:
+            build_metric(name)
+        except Exception as e:
+            raise e
+        print(f"Successfully built `{name}`")
 
 
 if __name__ == "__main__":
     build_models()
     build_preprocessors()
+    build_metrics()
+    build_datasets()
+    build_embeddings()
