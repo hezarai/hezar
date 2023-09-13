aa34cd031207d1c4f0c00039f0ffa0a229699f2e
arxyzan <arxyzan@gmail.com>
2023-09-13 17:31:21 +0330

    Add CER to metrics (#84)

--- hezar/metrics/__init__.py
+++ hezar/metrics/__init__.py
@@ -1,5 +1,6 @@
 from .f1 import F1, F1Config
 from .accuracy import Accuracy, AccuracyConfig
+from .cer import CER, CERConfig
 from .metric import Metric
 from .precision import Precision, PrecisionConfig
 from .recall import Recall, RecallConfig
---
+++ hezar/metrics/cer.py
@@ -0,0 +1,75 @@
+from dataclasses import dataclass
+
+import jiwer
+import jiwer.transforms as tr
+
+from ..configs import MetricConfig
+from ..constants import MetricType
+from ..registry import register_metric
+from .metric import Metric
+
+_DESCRIPTION = "Character Error Rate (CER) using `jiwer`. Commonly used for Speech Recognition systems"
+
+
+@dataclass
+class CERConfig(MetricConfig):
+    name = MetricType.CER
+    sentence_delimiter: str = " "
+    concatenate_texts: bool = False
+    output_keys: tuple = ("cer",)
+
+
+@register_metric("cer", config_class=CERConfig, description=_DESCRIPTION)
+class CER(Metric):
+    def __init__(self, config: CERConfig, **kwargs):
+        super().__init__(config=config, **kwargs)
+        self.transform = tr.Compose(
+            [
+                tr.RemoveMultipleSpaces(),
+                tr.Strip(),
+                tr.ReduceToSingleSentence(self.config.sentence_delimiter),
+                tr.ReduceToListOfListOfChars(),
+            ]
+        )
+
+    def compute(
+        self,
+        predictions=None,
+        targets=None,
+        concatenate_texts=None,
+        n_decimals=None,
+        output_keys=None,
+        **kwargs,
+    ):
+        concatenate_texts = concatenate_texts or self.config.concatenate_texts
+        n_decimals = n_decimals or self.config.n_decimals
+
+        if concatenate_texts:
+            score = jiwer.compute_measures(
+                targets,
+                predictions,
+                truth_transform=self.transform,
+                hypothesis_transform=self.transform,
+            )["wer"]
+
+        else:
+            incorrect = 0
+            total = 0
+            for prediction, reference in zip(predictions, targets):
+                measures = jiwer.compute_measures(
+                    reference,
+                    prediction,
+                    truth_transform=self.transform,
+                    hypothesis_transform=self.transform,
+                )
+                incorrect += measures["substitutions"] + measures["deletions"] + measures["insertions"]
+                total += measures["substitutions"] + measures["deletions"] + measures["hits"]
+
+            score = incorrect / total
+
+        results = {"cer": round(float(score), n_decimals)}
+
+        if output_keys:
+            results = {k: v for k, v in results.items() if k in output_keys}
+
+        return results
