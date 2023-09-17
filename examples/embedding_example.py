d99f0c9b2c33af313c9c2a116f67e5223d98c918
arxyzan <arxyzan@gmail.com>
2023-09-17 22:30:37 +0330

    Minor fix

--- examples/embedding_example.py
+++ examples/embedding_example.py
@@ -1,5 +1,5 @@
 from hezar import Embedding
 
-fasttext = Embedding.load("hezarai/word2vec-skipgram-fa-wikipedia")
+fasttext = Embedding.load("hezarai/fasttext-fa-300")
 most_similar = fasttext.most_similar("هزار")
 print(most_similar)
