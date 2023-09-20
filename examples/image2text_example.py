8f2f48672f0c21daee630d5d338e34414214fcec
arxyzan <arxyzan@gmail.com>
2023-09-20 23:40:27 +0330

    Update image2text_example.py

--- examples/image2text_example.py
+++ examples/image2text_example.py
@@ -1,5 +1,6 @@
 from hezar import Model
 
-model = Model.load("hezarai/trocr-fa-v1")
-text = model.predict(["assets/ocr_example.jpg"])
+# model = Model.load("hezarai/trocr-fa-v1")  # TrOCR
+model = Model.load("hezarai/crnn-base-fa-64x256")  # CRNN
+text = model.predict("assets/ocr_example.jpg")
 print(text)
