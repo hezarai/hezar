6dff9bc5d8b2f9e5de3b889893a36e9a0bd38ba2
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-10-15 11:41:14 +0200

    skip sorting base classes

--- hezar/preprocessors/tokenizers/__init__.py
+++ hezar/preprocessors/tokenizers/__init__.py
@@ -1,9 +1,9 @@
+from .tokenizer import Tokenizer, TokenizerConfig  # isort: skip
 from .bpe import BPEConfig, BPETokenizer
 from .sentencepiece_bpe import SentencePieceBPEConfig, SentencePieceBPETokenizer
 from .sentencepiece_unigram import (
     SentencePieceUnigramConfig,
     SentencePieceUnigramTokenizer,
 )
-from .tokenizer import Tokenizer, TokenizerConfig
 from .whisper_bpe import WhisperBPEConfig, WhisperBPETokenizer
 from .wordpiece import WordPieceConfig, WordPieceTokenizer
