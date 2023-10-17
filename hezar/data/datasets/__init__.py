417198e4f0ae9e304e95b91b83d5a507de5f8a6d
arxyzan <arxyzan@gmail.com>
2023-10-17 18:23:17 +0330

    Reformat imports

--- hezar/data/datasets/__init__.py
+++ hezar/data/datasets/__init__.py
@@ -1,13 +1,5 @@
 from .dataset import Dataset
-from .sequence_labeling_dataset import (
-    SequenceLabelingDataset,
-    SequenceLabelingDatasetConfig,
-)
-from .text_classification_dataset import (
-    TextClassificationDataset,
-    TextClassificationDatasetConfig,
-)
-from .text_summarization_dataset import (
-    TextSummarizationDataset,
-    TextSummarizationDatasetConfig,
-)
+from .ocr_dataset import OCRDataset, OCRDatasetConfig
+from .sequence_labeling_dataset import SequenceLabelingDataset, SequenceLabelingDatasetConfig
+from .text_classification_dataset import TextClassificationDataset, TextClassificationDatasetConfig
+from .text_summarization_dataset import TextSummarizationDataset, TextSummarizationDatasetConfig
