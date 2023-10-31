5265d6abaeb301bb04ad32e4c49835b79dd06f7e
arxyzan <arxyzan@gmail.com>
2023-10-31 11:03:14 +0330

    Refactor root imports

--- hezar/__init__.py
+++ hezar/__init__.py
@@ -1,13 +1,55 @@
-from .registry import *
-from .builders import *
-from .configs import *
-from .data import *
-from .embeddings import *
-from .metrics import *
-from .models import *
-from .preprocessors import *
-from .trainer import *
-from .utils import *
+"""
+Direct importing from hezar's root is no longer supported nor recommended since version 0.32.0. The following is just a
+workaround for backward compatibility. Any class, functions, etc. must be imported from its main submodule under hezar.
+"""
+
+import warnings
 
 
-__version__ = "0.32.1"
+def _warn_on_import(name: str, submodule: str):
+    warnings.warn(
+        f"Importing {name} from hezar root is deprecated and will be removed soon. "
+        f"Please use `from {submodule} import {name}`"
+    )
+
+
+def __getattr__(name: str):
+    if name == "Model":
+        from hezar.models import Model
+        _warn_on_import(name, "hezar.models")
+        return Model
+    elif name == "Dataset":
+        from .data import Dataset
+        _warn_on_import(name, "hezar.data")
+        return Dataset
+    elif name == "Trainer":
+        from .trainer import Trainer
+        _warn_on_import(name, "hezar.trainer")
+        return Trainer
+    elif name == "Embedding":
+        from .embeddings import Embedding
+        _warn_on_import(name, "hezar.embeddings")
+        return Embedding
+    elif name == "Preprocessor":
+        from .preprocessors import Preprocessor
+        _warn_on_import(name, "hezar.preprocessors")
+        return Preprocessor
+    elif name == "Metric":
+        from .metrics import Metric
+        _warn_on_import(name, "hezar.metrics")
+        return Metric
+    elif "Config" in name:
+        from .configs import Config
+        _warn_on_import(name, "hezar.configs")
+        return Config
+
+
+__all__ = [
+    "Config",
+    "Model",
+    "Dataset",
+    "Trainer",
+    "Preprocessor",
+    "Embedding",
+    "Metric",
+]
