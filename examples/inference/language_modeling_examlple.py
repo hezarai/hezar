75507e245bf73eb73fd0bc440781a71614088c6b
arxyzan <arxyzan@gmail.com>
2023-11-07 13:44:01 +0330

    Refactor language modeling outputs

--- examples/inference/language_modeling_examlple.py
+++ examples/inference/language_modeling_examlple.py
@@ -4,5 +4,5 @@ from hezar import Model
 path = "hezarai/roberta-fa-mlm"
 text = ["سلام بچه ها حالتون <mask>"]
 model = Model.load(path)
-outputs = model.predict(text)
+outputs = model.predict(text, top_k=1, device="cuda")
 print(outputs)
