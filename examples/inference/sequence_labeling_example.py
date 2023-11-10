211149e3ef8823132c2b621a9e1da05eac4ea0b7
arxyzan <arxyzan@gmail.com>
2023-11-10 19:50:06 +0330

    Remove sequence_labeling_example.py

---
+++ examples/inference/sequence_labeling_example.py
@@ -0,0 +1,8 @@
+from hezar.models import Model
+
+
+hub_path = "hezarai/bert-fa-pos-lscp-500k"
+model = Model.load(hub_path)
+inputs = ["سلام بر فارسی زبانان شریف"]
+outputs = model.predict(inputs)
+print(outputs)
