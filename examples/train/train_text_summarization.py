04803efb9b29a7795a4cb6b4f22616cfedd6ec32
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2023-11-11 21:27:57 +0330

    Rename text summarization

--- examples/train/train_text_summarization.py
+++ examples/train/train_text_summarization.py
@@ -3,12 +3,11 @@ from hezar.data import Dataset
 from hezar.preprocessors import Preprocessor
 from hezar.trainer import Trainer, TrainerConfig
 
-
 dataset_path = "hezarai/xlsum-fa"
 base_model_path = "hezarai/gpt2-base-fa"
 
-train_dataset = Dataset.load(dataset_path, split="train[:1%]", tokenizer_path=base_model_path)
-eval_dataset = Dataset.load(dataset_path, split="test[:1%]", tokenizer_path=base_model_path)
+train_dataset = Dataset.load(dataset_path, split="train", tokenizer_path=base_model_path)
+eval_dataset = Dataset.load(dataset_path, split="test", tokenizer_path=base_model_path)
 
 model = GPT2TextGeneration(GPT2TextGenerationConfig())
 preprocessor = Preprocessor.load(base_model_path)
@@ -18,9 +17,9 @@ train_config = TrainerConfig(
     device="cuda",
     init_weights_from=base_model_path,
     batch_size=8,
-    num_epochs=5,
+    num_epochs=10,
     checkpoints_dir="checkpoints/",
-    metrics=[],
+    metrics=["bleu", ],
 )
 
 trainer = Trainer(
