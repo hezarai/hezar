8e2320bf5dc82b6f0ec3b1c8063f605443d35da0
arxyzan <arxyzan@gmail.com>
2023-11-12 15:07:56 +0330

    Add `sanitize_params_for_fn` to utils

--- hezar/utils/common_utils.py
+++ hezar/utils/common_utils.py
@@ -1,10 +1,11 @@
 from re import sub
-from typing import Dict, List
-
+from typing import Dict, List, Callable, Union, Mapping
+import inspect
 
 __all__ = [
     "snake_case",
     "permute_dict_list",
+    "sanitize_params_for_fn",
 ]
 
 
@@ -26,3 +27,24 @@ def permute_dict_list(dict_list: List[Dict]) -> Dict[str, List]:
         return {}
     d = {key: [x[key] for x in dict_list] for key in dict_list[0]}
     return d
+
+
+def sanitize_params_for_fn(fn: Callable, params: Union[Dict, Mapping], **kwargs):
+    """
+    Given a dict of parameters or kwargs, you can figure out which ones must be passed to the `fn` based on its
+    signature.
+
+    Args:
+         fn: The function object
+         params: A dict of parameters with values
+         kwargs: Keyword arguments that are merged with `params`
+
+    Returns:
+        The proper dict of parameters keys and values
+    """
+    params.update(**kwargs)
+    params_signature = dict(inspect.signature(fn).parameters)
+    fn_parameters = {p for p, v in params_signature.items() if v.kind not in (v.VAR_KEYWORD, v.VAR_POSITIONAL)}
+    fn_params_names = set(fn_parameters)
+    input_params = {p: params[p] for p in fn_params_names if p in params}
+    return input_params
