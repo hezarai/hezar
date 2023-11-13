dca8c8d4bfb104ca8344e78a5dd04e405ee58584
arxyzan <arxyzan@gmail.com>
2023-11-14 00:04:19 +0330

    Remove pprint from `Embedding.most_similar()` output

--- examples/embedding/embedding_example.py
+++ examples/embedding/embedding_example.py
@@ -1,6 +1,9 @@
+from pprint import pprint
+
 from hezar.embeddings import Embedding
 
 
-embedding_model = Embedding.load("hezarai/word2vec-skipgram-fa-wikipedia")
+embedding_model = Embedding.load("hezarai/fasttext-fa-300")
 most_similar = embedding_model.most_similar("هزار")
-print(most_similar)
+doesnt_match = embedding_model.doesnt_match(["کامپیوتر", "مهندس", "نرم افزار", "دوغ"])
+pprint(most_similar)
