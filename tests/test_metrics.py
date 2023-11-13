65b3c3eaa7b5ba66156d91b4222dc14fc2d4da00
arxyzan <arxyzan@gmail.com>
2023-11-14 01:32:09 +0330

    Add `test_metrics.py`

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,60 +1,93 @@
 from unittest import TestCase
-
+from hezar.metrics import Metric
 from hezar.builders import build_metric
+
+INVALID_OUTPUT_TYPE = "Metric output must be a dictionary!"
+INVALID_FIELDS = "Invalid fields in metric outputs!"
 
 
 class MetricsTestCase(TestCase):
     def test_accuracy(self):
-        metric = build_metric("accuracy")
-        predictions = []
-        references = [[]]
+        metric: Metric = build_metric("accuracy")
+        predictions = [0, 2, 1, 3]
+        references = [0, 1, 2, 3]
         results = metric.compute(predictions, references)
-
+        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
+        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
 
     def test_bleu(self):
-        metric = build_metric("bleu")
-        predictions = []
-        references = [[]]
+        metric: Metric = build_metric("bleu")
+        pred_1 = ["It", "is", "a", "guide", "to", "action", "which", "ensures", "that", "the", "military", "always",
+                  "obeys", "the", "commands", "of", "the", "party"]
+        ref_1a = ["It", "is", "a", "guide", "to", "action", "that", "ensures", "that", "the", "military", "will",
+                  "forever", "heed", "Party", "commands"]
+        ref_1b = ["It", "is", "the", "guiding", "principle", "which", "guarantees", "the", "military", "forces",
+                  "always", "being", "under", "the", "command", "of", "the", "Party"]
+        ref_1c = ["It", "is", "the", "practical", "guide", "for", "the", "army", "always", "to", "heed", "the",
+                  "directions", "of", "the", "party"]
+        pred_2 = ["he", "read", "the", "book", "because", "he", "was", "interested", "in", "world", "history"]
+        ref_2a = ["he", "was", "interested", "in", "world", "history", "because", "he", "read", "the", "book"]
+
+        references = [[ref_1a, ref_1b, ref_1c], [ref_2a]]
+        predictions = [pred_1, pred_2]
+
         results = metric.compute(predictions, references)
+
+        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
+        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
 
     def test_cer(self):
-        metric = build_metric("cer")
-        predictions = []
-        references = [[]]
+        metric: Metric = build_metric("cer")
+        predictions = ["بهتره این یکی رو بیخیال شیم"]
+        references = ["بهتره این یکی رو بیخیال بشیم"]
         results = metric.compute(predictions, references)
+        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
+        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
 
     def test_f1(self):
-        metric = build_metric("f1")
-        predictions = []
-        references = [[]]
+        metric: Metric = build_metric("f1")
+        predictions = [0, 1, 2, 0, 1, 2]
+        references = [0, 2, 1, 0, 0, 1]
         results = metric.compute(predictions, references)
+        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
+        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
 
     def test_precision(self):
-        metric = build_metric("precision")
-        predictions = []
-        references = [[]]
+        metric: Metric = build_metric("precision")
+        predictions = [0, 1, 2, 0, 1, 2]
+        references = [0, 2, 1, 0, 0, 1]
         results = metric.compute(predictions, references)
+        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
+        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
 
     def test_recall(self):
-        metric = build_metric("recall")
-        predictions = []
-        references = [[]]
+        metric: Metric = build_metric("recall")
+        predictions = [0, 1, 2, 0, 1, 2]
+        references = [0, 2, 1, 0, 0, 1]
         results = metric.compute(predictions, references)
+        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
+        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
 
     def test_rouge(self):
-        metric = build_metric("rouge")
-        predictions = []
-        references = [[]]
+        metric: Metric = build_metric("rouge")
+        predictions = ["از این معیار برای سنجش خروجی مدل های ترجمه یا خلاصه سازی استفاده می شود"]
+        references = ["در واقع از این معیار برای سنجش خروجی مدل های ترجمه یا خلاصه سازی استفاده می شود"]
         results = metric.compute(predictions, references)
+        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
+        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
 
     def test_seqeval(self):
-        metric = build_metric("seqeval")
-        predictions = []
-        references = [[]]
+        metric: Metric = build_metric("seqeval")
+        predictions = [['O', 'O', 'O', 'B-MISC', 'I-MISC', 'I-MISC', 'O'], ['B-PER', 'I-PER', 'O']]
+        references = [['O', 'O', 'B-MISC', 'I-MISC', 'I-MISC', 'I-MISC', 'O'], ['B-PER', 'I-PER', 'O']]
         results = metric.compute(predictions, references)
+        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
+        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
 
     def test_wer(self):
-        metric = build_metric("wer")
-        predictions = []
-        references = [[]]
+        metric: Metric = build_metric("wer")
+        predictions = ["بهتره این یکی رو بیخیال شیم"]
+        references = ["بهتره این یکی رو بیخیال بشیم"]
         results = metric.compute(predictions, references)
+        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
+        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
