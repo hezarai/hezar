d6c79c5089dbf0970c7ce3cd0af12b02b7560930
arxyzan <arxyzan@gmail.com>
2023-11-14 15:30:41 +0330

    Update T5 text summarization training script

---
+++ examples/train/train_t5_text_summarization.py
@@ -0,0 +1,51 @@
+from hezar.models import T5TextGeneration, T5TextGenerationConfig
+from hezar.data import Dataset
+from hezar.preprocessors import Preprocessor
+from hezar.trainer import Trainer, TrainerConfig
+
+
+if __name__ == '__main__':
+    dataset_path = "hezarai/xlsum-fa"
+    base_model_path = "hezarai/t5-base-fa"
+    PREFIX = "این متن را خلاصه کن: "
+
+    train_dataset = Dataset.load(
+        dataset_path,
+        split="train",
+        tokenizer_path=base_model_path,
+        max_length=384,
+        max_target_length=80,
+        prefix=PREFIX
+    )
+    eval_dataset = Dataset.load(
+        dataset_path,
+        split="test",
+        tokenizer_path=base_model_path,
+        max_length=384,
+        max_target_length=80,
+        prefix=PREFIX,
+    )
+
+    model = T5TextGeneration(T5TextGenerationConfig())
+    preprocessor = Preprocessor.load(base_model_path)
+
+    train_config = TrainerConfig(
+        task="text_generation",
+        device="cuda",
+        init_weights_from=base_model_path,
+        batch_size=8,
+        eval_batch_size=64,
+        num_epochs=10,
+        checkpoints_dir="checkpoints/",
+        metrics=["rouge"],
+    )
+
+    trainer = Trainer(
+        config=train_config,
+        model=model,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        data_collator=train_dataset.data_collator,
+        preprocessor=preprocessor,
+    )
+    trainer.evaluate()
