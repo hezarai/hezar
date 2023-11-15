7a32fd6a4a3ead5169f28638644f1a7f5ce86850
arxyzan <arxyzan@gmail.com>
2023-11-15 13:37:58 +0330

    Add dataloaders to `test_datasets.py`

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -17,11 +17,17 @@ TASK_TO_TOKENIZER_MAPPING = {
     "text-summarization": "hezarai/t5-base-fa",
 }
 
-INVALID_DATASET_TYPE = "Wrong dataset type!"
+INVALID_DATASET_TYPE = "Dataset instance must be of type `Dataset`, got `{}`!"
+INVALID_BATCH_TYPE = "A data batch must be a dictionary not `{}`!"
 INVALID_DATASET_FIELDS = "Dataset samples must have the field `{}`!"
 
 
 class HubDatasetsTestCase(TestCase):
+    @staticmethod
+    def _create_dataloader(dataset, batch_size, shuffle, collate_fn):
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
+        return dataloader
+
     def test_load_text_classification(self):
         required_fields = ["token_ids", "attention_mask", "labels"]
 
@@ -30,7 +36,7 @@ class HubDatasetsTestCase(TestCase):
             split="train",
             tokenizer_path=TASK_TO_TOKENIZER_MAPPING["text-classification"]
         )
-        self.assertIsInstance(train_dataset, Dataset, INVALID_DATASET_TYPE)
+        self.assertIsInstance(train_dataset, Dataset, INVALID_DATASET_TYPE.format(type(train_dataset)))
         sample = train_dataset[0]
         self.assertIsInstance(sample, Dict)
         [self.assertIn(x, sample, INVALID_DATASET_FIELDS.format(x)) for x in required_fields]
@@ -40,10 +46,29 @@ class HubDatasetsTestCase(TestCase):
             split="test",
             tokenizer_path=TASK_TO_TOKENIZER_MAPPING["text-classification"]
         )
-        self.assertIsInstance(test_dataset, Dataset, INVALID_DATASET_TYPE)
+        self.assertIsInstance(test_dataset, Dataset, INVALID_DATASET_TYPE.format(type(test_dataset)))
         sample = test_dataset[0]
         self.assertIsInstance(sample, Dict)
         [self.assertIn(x, sample, INVALID_DATASET_FIELDS.format(x)) for x in required_fields]
+
+        # Dataloaders
+        train_loader = self._create_dataloader(
+            train_dataset,
+            batch_size=8,
+            shuffle=True,
+            collate_fn=train_dataset.data_collator,
+        )
+        test_loader = self._create_dataloader(
+            test_dataset,
+            batch_size=8,
+            shuffle=True,
+            collate_fn=test_dataset.data_collator,
+        )
+        train_batch = next(iter(train_loader))
+        test_batch = next(iter(test_loader))
+
+        self.assertIsInstance(train_batch, Dict, INVALID_BATCH_TYPE.format(type(train_batch)))
+        self.assertIsInstance(test_batch, Dict, INVALID_BATCH_TYPE.format(type(test_batch)))
 
     def test_load_sequence_labeling(self):
         required_fields = ["token_ids", "attention_mask", "special_tokens_mask", "word_ids", "labels"]
@@ -68,6 +93,25 @@ class HubDatasetsTestCase(TestCase):
         self.assertIsInstance(sample, Dict)
         [self.assertIn(x, sample, INVALID_DATASET_FIELDS.format(x)) for x in required_fields]
 
+        # Dataloaders
+        train_loader = self._create_dataloader(
+            train_dataset,
+            batch_size=8,
+            shuffle=True,
+            collate_fn=train_dataset.data_collator,
+        )
+        test_loader = self._create_dataloader(
+            test_dataset,
+            batch_size=8,
+            shuffle=True,
+            collate_fn=test_dataset.data_collator,
+        )
+        train_batch = next(iter(train_loader))
+        test_batch = next(iter(test_loader))
+
+        self.assertIsInstance(train_batch, Dict, INVALID_BATCH_TYPE.format(type(train_batch)))
+        self.assertIsInstance(test_batch, Dict, INVALID_BATCH_TYPE.format(type(test_batch)))
+
     def test_load_ocr(self):
         required_fields = ["pixel_values", "labels"]
 
@@ -91,6 +135,25 @@ class HubDatasetsTestCase(TestCase):
         self.assertIsInstance(sample, Dict)
         [self.assertIn(x, sample, INVALID_DATASET_FIELDS.format(x)) for x in required_fields]
 
+        # Dataloaders
+        train_loader = self._create_dataloader(
+            train_dataset,
+            batch_size=8,
+            shuffle=True,
+            collate_fn=train_dataset.data_collator,
+        )
+        test_loader = self._create_dataloader(
+            test_dataset,
+            batch_size=8,
+            shuffle=True,
+            collate_fn=test_dataset.data_collator,
+        )
+        train_batch = next(iter(train_loader))
+        test_batch = next(iter(test_loader))
+
+        self.assertIsInstance(train_batch, Dict, INVALID_BATCH_TYPE.format(type(train_batch)))
+        self.assertIsInstance(test_batch, Dict, INVALID_BATCH_TYPE.format(type(test_batch)))
+
     def test_load_text_summarization(self):
         required_fields = ["token_ids", "attention_mask", "labels"]
 
@@ -113,3 +176,22 @@ class HubDatasetsTestCase(TestCase):
         sample = test_dataset[0]
         self.assertIsInstance(sample, Dict)
         [self.assertIn(x, sample, INVALID_DATASET_FIELDS.format(x)) for x in required_fields]
+
+        # Dataloaders
+        train_loader = self._create_dataloader(
+            train_dataset,
+            batch_size=8,
+            shuffle=True,
+            collate_fn=train_dataset.data_collator,
+        )
+        test_loader = self._create_dataloader(
+            test_dataset,
+            batch_size=8,
+            shuffle=True,
+            collate_fn=test_dataset.data_collator,
+        )
+        train_batch = next(iter(train_loader))
+        test_batch = next(iter(test_loader))
+
+        self.assertIsInstance(train_batch, Dict, INVALID_BATCH_TYPE.format(type(train_batch)))
+        self.assertIsInstance(test_batch, Dict, INVALID_BATCH_TYPE.format(type(test_batch)))
