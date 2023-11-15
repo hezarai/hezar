b6312543a1d12e82d07c34384304efa2793c966a
arxyzan <arxyzan@gmail.com>
2023-11-15 17:52:25 +0330

    Refactor tests: `unittext` -> `pytest`

--- tests/test_embeddings.py
+++ tests/test_embeddings.py
@@ -1,6 +1,5 @@
-from unittest import TestCase
-
 from hezar.embeddings import Embedding
+import pytest
 
 TESTABLE_EMBEDDINGS = {
     "word2vec-skipgram": "hezarai/word2vec-skipgram-fa-wikipedia",
@@ -10,39 +9,17 @@ TESTABLE_EMBEDDINGS = {
 MOST_SIMILAR_TOP_N = 5
 
 
-class EmbeddingsTestCase(TestCase):
-    def test_word2vec_skipgram(self):
-        embedding_model = Embedding.load(TESTABLE_EMBEDDINGS["word2vec-skipgram"])
-        most_similar = embedding_model.most_similar("هزار", top_n=MOST_SIMILAR_TOP_N)
-        self.assertEqual(type(most_similar), list)
-        self.assertEqual(len(most_similar), MOST_SIMILAR_TOP_N)
-        self.assertEqual(
-            {k for x in most_similar for k in x.keys()},
-            {"word", "score"}
-        )
-        doesnt_match = embedding_model.doesnt_match(["کامپیوتر", "مهندس", "نرم افزار", "دوغ"])
-        self.assertEqual(type(doesnt_match), str)
+@pytest.mark.parametrize("embedding_key", TESTABLE_EMBEDDINGS.keys())
+def test_embedding_most_similar(embedding_key):
+    embedding_model = Embedding.load(TESTABLE_EMBEDDINGS[embedding_key])
+    most_similar = embedding_model.most_similar("هزار", top_n=MOST_SIMILAR_TOP_N)
+    assert isinstance(most_similar, list)
+    assert len(most_similar) == MOST_SIMILAR_TOP_N
+    assert {k for x in most_similar for k in x.keys()} == {"word", "score"}
 
-    def test_word2vec_cbow(self):
-        embedding_model = Embedding.load(TESTABLE_EMBEDDINGS["word2vec-cbow"])
-        most_similar = embedding_model.most_similar("هزار", top_n=MOST_SIMILAR_TOP_N)
-        self.assertEqual(type(most_similar), list)
-        self.assertEqual(len(most_similar), MOST_SIMILAR_TOP_N)
-        self.assertEqual(
-            {k for x in most_similar for k in x.keys()},
-            {"word", "score"}
-        )
-        doesnt_match = embedding_model.doesnt_match(["کامپیوتر", "مهندس", "نرم افزار", "دوغ"])
-        self.assertEqual(type(doesnt_match), str)
 
-    def test_fasttext(self):
-        embedding_model = Embedding.load(TESTABLE_EMBEDDINGS["fasttext"])
-        most_similar = embedding_model.most_similar("هزار", top_n=MOST_SIMILAR_TOP_N)
-        self.assertEqual(type(most_similar), list)
-        self.assertEqual(len(most_similar), MOST_SIMILAR_TOP_N)
-        self.assertEqual(
-            {k for x in most_similar for k in x.keys()},
-            {"word", "score"}
-        )
-        doesnt_match = embedding_model.doesnt_match(["کامپیوتر", "مهندس", "نرم افزار", "دوغ"])
-        self.assertEqual(type(doesnt_match), str)
+@pytest.mark.parametrize("embedding_key", TESTABLE_EMBEDDINGS.keys())
+def test_embedding_doesnt_match(embedding_key):
+    embedding_model = Embedding.load(TESTABLE_EMBEDDINGS[embedding_key])
+    doesnt_match = embedding_model.doesnt_match(["کامپیوتر", "مهندس", "نرم افزار", "دوغ"])
+    assert isinstance(doesnt_match, str)
--- tests/test_models_inference.py
+++ tests/test_models_inference.py
@@ -1,8 +1,6 @@
 import os
-from unittest import TestCase
 
 from hezar.models import Model
-
 
 TESTABLE_MODELS = {
     "automatic-speech-recognition": "hezarai/whisper-small-fa",
@@ -14,80 +12,74 @@ TESTABLE_MODELS = {
     "sequence-labeling": "hezarai/bert-fa-pos-lscp-500k",
 }
 
-# Assertion messages
 INVALID_OUTPUT_TYPE = "Model output must be a batch!"
 INVALID_OUTPUT_SIZE = "Model output must be a list of size 1!"
 INVALID_OUTPUT_FIELDS = "Invalid fields in the model outputs!"
 
 
-class ModelsInferenceTestCase(TestCase):
-    def test_text_classification(self):
-        inputs = ["هزار، کتابخانه‌ای کامل برای به کارگیری آسان هوش مصنوعی"]
-        model = Model.load(TESTABLE_MODELS["text-classification"])
-        outputs = model.predict(inputs, top_k=2)
-        self.assertEqual(type(outputs), list)
-        self.assertEqual(len(outputs), 1)
-        self.assertEqual(type(outputs[0]), list)
-        self.assertEqual(
-            {k for x in outputs[0] for k in x.keys()},
-            {"label", "score"},
-        )
+def test_text_classification():
+    inputs = ["هزار، کتابخانه‌ای کامل برای به کارگیری آسان هوش مصنوعی"]
+    model = Model.load(TESTABLE_MODELS["text-classification"])
+    outputs = model.predict(inputs, top_k=2)
+    assert isinstance(outputs, list)
+    assert len(outputs) == 1
+    assert isinstance(outputs[0], list)
+    assert {k for x in outputs[0] for k in x.keys()} == {"label", "score"}
 
-    def test_sequence_labeling(self):
-        inputs = ["شرکت هوش مصنوعی هزار"]
-        model = Model.load(TESTABLE_MODELS["sequence-labeling"])
-        outputs = model.predict(inputs, return_scores=True, return_offsets=True)
-        self.assertEqual(type(outputs), list)
-        self.assertEqual(len(outputs), 1)
-        self.assertEqual(type(outputs[0]), list)
-        self.assertEqual(
-            {k for el in outputs[0] for k in el.keys()},
-            {"label", "token", "start", "end", "score"},
-        )
 
-    def test_automatic_speech_recognition(self):
-        dirname = os.path.dirname(os.path.abspath(__file__))
-        inputs = os.path.join(dirname, "samples", "speech_example.mp3")
-        model = Model.load(TESTABLE_MODELS["automatic-speech-recognition"])
-        outputs = model.predict(inputs)
-        self.assertEqual(type(outputs), list, INVALID_OUTPUT_TYPE)
-        self.assertEqual(len(outputs), 1, INVALID_OUTPUT_SIZE)
-        self.assertEqual(outputs[0].keys(), ["text", "chunks"], INVALID_OUTPUT_FIELDS)
+def test_sequence_labeling():
+    inputs = ["شرکت هوش مصنوعی هزار"]
+    model = Model.load(TESTABLE_MODELS["sequence-labeling"])
+    outputs = model.predict(inputs, return_scores=True, return_offsets=True)
+    assert isinstance(outputs, list)
+    assert len(outputs) == 1
+    assert isinstance(outputs[0], list)
+    assert {k for el in outputs[0] for k in el.keys()} == {"label", "token", "start", "end", "score"}
 
-    def test_masked_language_modeling(self):
-        inputs = ["سلام بچه ها حالتون <mask>"]
-        model = Model.load(TESTABLE_MODELS["fill-mask"])
-        outputs = model.predict(inputs)
-        self.assertEqual(type(outputs), list, INVALID_OUTPUT_TYPE)
-        self.assertEqual(len(outputs), 1, INVALID_OUTPUT_SIZE)
-        self.assertEqual(
-            {k for x in outputs[0] for k in x.keys()},
-            {"token", "sequence", "token_id", "score"},
-            INVALID_OUTPUT_FIELDS,
-        )
 
-    def test_text_generation(self):
-        inputs = ["با پیشرفت اخیر هوش مصنوعی در سال های اخیر، "]
-        model = Model.load(TESTABLE_MODELS["text-generation"])
-        outputs = model.predict(inputs)
-        self.assertEqual(type(outputs), list, INVALID_OUTPUT_TYPE)
-        self.assertEqual(len(outputs), 1, INVALID_OUTPUT_SIZE)
-        self.assertEqual(outputs[0].keys(), ["text"], INVALID_OUTPUT_FIELDS)
+def test_automatic_speech_recognition():
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    inputs = os.path.join(dirname, "samples", "speech_example.mp3")
+    model = Model.load(TESTABLE_MODELS["automatic-speech-recognition"])
+    outputs = model.predict(inputs)
+    assert isinstance(outputs, list), INVALID_OUTPUT_TYPE
+    assert len(outputs) == 1, INVALID_OUTPUT_SIZE
+    assert set(outputs[0].keys()) == {"text", "chunks"}, INVALID_OUTPUT_FIELDS
 
-    def test_ocr(self):
-        dirname = os.path.dirname(os.path.abspath(__file__))
-        inputs = os.path.join(dirname, "samples", "ocr_example.jpg")
-        model = Model.load(TESTABLE_MODELS["ocr"])
-        outputs = model.predict(inputs)
-        self.assertEqual(type(outputs), list, INVALID_OUTPUT_TYPE)
-        self.assertEqual(len(outputs), 1, INVALID_OUTPUT_SIZE)
-        self.assertEqual(outputs[0].keys(), ["text"], INVALID_OUTPUT_FIELDS)
 
-    def test_image_captioning(self):
-        dirname = os.path.dirname(os.path.abspath(__file__))
-        inputs = os.path.join(dirname, "samples", "image_captioning_example.jpg")
-        model = Model.load(TESTABLE_MODELS["image-captioning"])
-        outputs = model.predict(inputs)
-        self.assertEqual(type(outputs), list, INVALID_OUTPUT_TYPE)
-        self.assertEqual(len(outputs), 1, INVALID_OUTPUT_SIZE)
-        self.assertEqual(outputs[0].keys(), ["text"], INVALID_OUTPUT_FIELDS)
+def test_masked_language_modeling():
+    inputs = ["سلام بچه ها حالتون <mask>"]
+    model = Model.load(TESTABLE_MODELS["fill-mask"])
+    outputs = model.predict(inputs)
+    assert isinstance(outputs, list), INVALID_OUTPUT_TYPE
+    assert len(outputs) == 1, INVALID_OUTPUT_SIZE
+    assert {k for x in outputs[0] for k in x.keys()} == {"token", "sequence", "token_id", "score"}, INVALID_OUTPUT_FIELDS
+
+
+def test_text_generation():
+    inputs = ["با پیشرفت اخیر هوش مصنوعی در سال های اخیر، "]
+    model = Model.load(TESTABLE_MODELS["text-generation"])
+    outputs = model.predict(inputs)
+    assert isinstance(outputs, list), INVALID_OUTPUT_TYPE
+    assert len(outputs) == 1, INVALID_OUTPUT_SIZE
+    assert set(outputs[0].keys()) == {"text"}, INVALID_OUTPUT_FIELDS
+
+
+def test_ocr():
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    inputs = os.path.join(dirname, "samples", "ocr_example.jpg")
+    model = Model.load(TESTABLE_MODELS["ocr"])
+    outputs = model.predict(inputs)
+    assert isinstance(outputs, list), INVALID_OUTPUT_TYPE
+    assert len(outputs) == 1, INVALID_OUTPUT_SIZE
+    assert set(outputs[0].keys()) == {"text"}, INVALID_OUTPUT_FIELDS
+
+
+def test_image_captioning():
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    inputs = os.path.join(dirname, "samples", "image_captioning_example.jpg")
+    model = Model.load(TESTABLE_MODELS["image-captioning"])
+    outputs = model.predict(inputs)
+    assert isinstance(outputs, list), INVALID_OUTPUT_TYPE
+    assert len(outputs) == 1, INVALID_OUTPUT_SIZE
+    assert set(outputs[0].keys()) == {"text"}, INVALID_OUTPUT_FIELDS
