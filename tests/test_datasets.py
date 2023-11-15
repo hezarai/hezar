8502dff31392e8e0b17283bf982bc1270c949faf
arxyzan <arxyzan@gmail.com>
2023-11-15 17:52:25 +0330

    Refactor tests: `unittext` -> `pytest`

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,8 +1,7 @@
-from unittest import TestCase
 from typing import Dict
-
 from torch.utils.data import DataLoader
 from hezar.data import Dataset
+import pytest
 
 TASK_TO_HUB_MAPPING = {
     "text-classification": "hezarai/sentiment-dksf",
@@ -17,181 +16,64 @@ TASK_TO_TOKENIZER_MAPPING = {
     "text-summarization": "hezarai/t5-base-fa",
 }
 
+TASK_TO_REQUIRED_FIELDS = {
+    "text-classification": ["token_ids", "attention_mask", "labels"],
+    "sequence-labeling": ["token_ids", "attention_mask", "special_tokens_mask", "word_ids", "labels"],
+    "ocr": ["pixel_values", "labels"],
+    "text-summarization": ["token_ids", "attention_mask", "labels"],
+}
+
 INVALID_DATASET_TYPE = "Dataset instance must be of type `Dataset`, got `{}`!"
 INVALID_BATCH_TYPE = "A data batch must be a dictionary not `{}`!"
 INVALID_DATASET_FIELDS = "Dataset samples must have the field `{}`!"
 
 
-class HubDatasetsTestCase(TestCase):
-    @staticmethod
-    def _create_dataloader(dataset, batch_size, shuffle, collate_fn):
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
-        return dataloader
+def create_dataloader(dataset, batch_size, shuffle, collate_fn):
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
+    return dataloader
 
-    def test_load_text_classification(self):
-        required_fields = ["token_ids", "attention_mask", "labels"]
 
-        train_dataset = Dataset.load(
-            TASK_TO_HUB_MAPPING["text-classification"],
-            split="train",
-            tokenizer_path=TASK_TO_TOKENIZER_MAPPING["text-classification"]
-        )
-        self.assertIsInstance(train_dataset, Dataset, INVALID_DATASET_TYPE.format(type(train_dataset)))
-        sample = train_dataset[0]
-        self.assertIsInstance(sample, Dict)
-        [self.assertIn(x, sample, INVALID_DATASET_FIELDS.format(x)) for x in required_fields]
+@pytest.mark.parametrize("task", TASK_TO_HUB_MAPPING.keys())
+def test_load_dataset(task):
+    required_fields = TASK_TO_REQUIRED_FIELDS[task]
 
-        test_dataset = Dataset.load(
-            TASK_TO_HUB_MAPPING["text-classification"],
-            split="test",
-            tokenizer_path=TASK_TO_TOKENIZER_MAPPING["text-classification"]
-        )
-        self.assertIsInstance(test_dataset, Dataset, INVALID_DATASET_TYPE.format(type(test_dataset)))
-        sample = test_dataset[0]
-        self.assertIsInstance(sample, Dict)
-        [self.assertIn(x, sample, INVALID_DATASET_FIELDS.format(x)) for x in required_fields]
+    train_dataset = Dataset.load(
+        TASK_TO_HUB_MAPPING[task],
+        split="train",
+        tokenizer_path=TASK_TO_TOKENIZER_MAPPING[task]
+    )
+    assert isinstance(train_dataset, Dataset), INVALID_DATASET_TYPE.format(type(train_dataset))
+    sample = train_dataset[0]
+    assert isinstance(sample, Dict)
+    for field in required_fields:
+        assert field in sample, INVALID_DATASET_FIELDS.format(field)
 
-        # Dataloaders
-        train_loader = self._create_dataloader(
-            train_dataset,
-            batch_size=8,
-            shuffle=True,
-            collate_fn=train_dataset.data_collator,
-        )
-        test_loader = self._create_dataloader(
-            test_dataset,
-            batch_size=8,
-            shuffle=True,
-            collate_fn=test_dataset.data_collator,
-        )
-        train_batch = next(iter(train_loader))
-        test_batch = next(iter(test_loader))
+    test_dataset = Dataset.load(
+        TASK_TO_HUB_MAPPING[task],
+        split="test",
+        tokenizer_path=TASK_TO_TOKENIZER_MAPPING[task]
+    )
+    assert isinstance(test_dataset, Dataset), INVALID_DATASET_TYPE.format(type(test_dataset))
+    sample = test_dataset[0]
+    assert isinstance(sample, Dict)
+    for field in required_fields:
+        assert field in sample, INVALID_DATASET_FIELDS.format(field)
 
-        self.assertIsInstance(train_batch, Dict, INVALID_BATCH_TYPE.format(type(train_batch)))
-        self.assertIsInstance(test_batch, Dict, INVALID_BATCH_TYPE.format(type(test_batch)))
+    # Dataloaders
+    train_loader = create_dataloader(
+        train_dataset,
+        batch_size=8,
+        shuffle=True,
+        collate_fn=train_dataset.data_collator,
+    )
+    test_loader = create_dataloader(
+        test_dataset,
+        batch_size=8,
+        shuffle=True,
+        collate_fn=test_dataset.data_collator,
+    )
+    train_batch = next(iter(train_loader))
+    test_batch = next(iter(test_loader))
 
-    def test_load_sequence_labeling(self):
-        required_fields = ["token_ids", "attention_mask", "special_tokens_mask", "word_ids", "labels"]
-
-        train_dataset = Dataset.load(
-            TASK_TO_HUB_MAPPING["sequence-labeling"],
-            split="train",
-            tokenizer_path=TASK_TO_TOKENIZER_MAPPING["sequence-labeling"]
-        )
-        self.assertIsInstance(train_dataset, Dataset, INVALID_DATASET_TYPE.format(type(train_dataset)))
-        sample = train_dataset[0]
-        self.assertIsInstance(sample, Dict)
-        [self.assertIn(x, sample, INVALID_DATASET_FIELDS.format(x)) for x in required_fields]
-
-        test_dataset = Dataset.load(
-            TASK_TO_HUB_MAPPING["sequence-labeling"],
-            split="test",
-            tokenizer_path=TASK_TO_TOKENIZER_MAPPING["sequence-labeling"]
-        )
-        self.assertIsInstance(test_dataset, Dataset, INVALID_DATASET_TYPE.format(type(test_dataset)))
-        sample = test_dataset[0]
-        self.assertIsInstance(sample, Dict)
-        [self.assertIn(x, sample, INVALID_DATASET_FIELDS.format(x)) for x in required_fields]
-
-        # Dataloaders
-        train_loader = self._create_dataloader(
-            train_dataset,
-            batch_size=8,
-            shuffle=True,
-            collate_fn=train_dataset.data_collator,
-        )
-        test_loader = self._create_dataloader(
-            test_dataset,
-            batch_size=8,
-            shuffle=True,
-            collate_fn=test_dataset.data_collator,
-        )
-        train_batch = next(iter(train_loader))
-        test_batch = next(iter(test_loader))
-
-        self.assertIsInstance(train_batch, Dict, INVALID_BATCH_TYPE.format(type(train_batch)))
-        self.assertIsInstance(test_batch, Dict, INVALID_BATCH_TYPE.format(type(test_batch)))
-
-    def test_load_ocr(self):
-        required_fields = ["pixel_values", "labels"]
-
-        train_dataset = Dataset.load(
-            TASK_TO_HUB_MAPPING["ocr"],
-            split="train",
-            tokenizer_path=TASK_TO_TOKENIZER_MAPPING["ocr"]
-        )
-        self.assertIsInstance(train_dataset, Dataset, INVALID_DATASET_TYPE.format(type(train_dataset)))
-        sample = train_dataset[0]
-        self.assertIsInstance(sample, Dict)
-        [self.assertIn(x, sample, INVALID_DATASET_FIELDS.format(x)) for x in required_fields]
-
-        test_dataset = Dataset.load(
-            TASK_TO_HUB_MAPPING["ocr"],
-            split="test",
-            tokenizer_path=TASK_TO_TOKENIZER_MAPPING["ocr"]
-        )
-        self.assertIsInstance(test_dataset, Dataset, INVALID_DATASET_TYPE.format(type(test_dataset)))
-        sample = test_dataset[0]
-        self.assertIsInstance(sample, Dict)
-        [self.assertIn(x, sample, INVALID_DATASET_FIELDS.format(x)) for x in required_fields]
-
-        # Dataloaders
-        train_loader = self._create_dataloader(
-            train_dataset,
-            batch_size=8,
-            shuffle=True,
-            collate_fn=train_dataset.data_collator,
-        )
-        test_loader = self._create_dataloader(
-            test_dataset,
-            batch_size=8,
-            shuffle=True,
-            collate_fn=test_dataset.data_collator,
-        )
-        train_batch = next(iter(train_loader))
-        test_batch = next(iter(test_loader))
-
-        self.assertIsInstance(train_batch, Dict, INVALID_BATCH_TYPE.format(type(train_batch)))
-        self.assertIsInstance(test_batch, Dict, INVALID_BATCH_TYPE.format(type(test_batch)))
-
-    def test_load_text_summarization(self):
-        required_fields = ["token_ids", "attention_mask", "labels"]
-
-        train_dataset = Dataset.load(
-            TASK_TO_HUB_MAPPING["text-summarization"],
-            split="train",
-            tokenizer_path=TASK_TO_TOKENIZER_MAPPING["text-summarization"]
-        )
-        self.assertIsInstance(train_dataset, Dataset, INVALID_DATASET_TYPE.format(type(train_dataset)))
-        sample = train_dataset[0]
-        self.assertIsInstance(sample, Dict)
-        [self.assertIn(x, sample, INVALID_DATASET_FIELDS.format(x)) for x in required_fields]
-
-        test_dataset = Dataset.load(
-            TASK_TO_HUB_MAPPING["text-summarization"],
-            split="test",
-            tokenizer_path=TASK_TO_TOKENIZER_MAPPING["text-summarization"]
-        )
-        self.assertIsInstance(test_dataset, Dataset, INVALID_DATASET_TYPE.format(type(test_dataset)))
-        sample = test_dataset[0]
-        self.assertIsInstance(sample, Dict)
-        [self.assertIn(x, sample, INVALID_DATASET_FIELDS.format(x)) for x in required_fields]
-
-        # Dataloaders
-        train_loader = self._create_dataloader(
-            train_dataset,
-            batch_size=8,
-            shuffle=True,
-            collate_fn=train_dataset.data_collator,
-        )
-        test_loader = self._create_dataloader(
-            test_dataset,
-            batch_size=8,
-            shuffle=True,
-            collate_fn=test_dataset.data_collator,
-        )
-        train_batch = next(iter(train_loader))
-        test_batch = next(iter(test_loader))
-
-        self.assertIsInstance(train_batch, Dict, INVALID_BATCH_TYPE.format(type(train_batch)))
-        self.assertIsInstance(test_batch, Dict, INVALID_BATCH_TYPE.format(type(test_batch)))
+    assert isinstance(train_batch, Dict), INVALID_BATCH_TYPE.format(type(train_batch))
+    assert isinstance(test_batch, Dict), INVALID_BATCH_TYPE.format(type(test_batch))
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,5 +1,5 @@
-from unittest import TestCase, skipIf
-
+import os
+import pytest
 from hezar.utils import list_available_metrics
 from hezar.builders import build_metric
 
@@ -7,97 +7,104 @@ INVALID_OUTPUT_TYPE = "Metric output must be a dictionary!"
 INVALID_FIELDS = "Invalid fields in metric outputs!"
 
 
-class MetricsTestCase(TestCase):
-    @skipIf("accuracy" not in list_available_metrics(), "`accuracy` is not available!")
-    def test_accuracy(self):
-        metric = build_metric("accuracy")
-        predictions = [0, 2, 1, 3]
-        references = [0, 1, 2, 3]
-        results = metric.compute(predictions, references)
-        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
-        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
+@pytest.mark.skipif("accuracy" not in list_available_metrics(), reason="`accuracy` is not available!")
+def test_accuracy():
+    metric = build_metric("accuracy")
+    predictions = [0, 2, 1, 3]
+    references = [0, 1, 2, 3]
+    results = metric.compute(predictions, references)
+    assert isinstance(results, dict), INVALID_OUTPUT_TYPE
+    assert set(results.keys()) == set(metric.config.output_keys), INVALID_FIELDS
 
-    @skipIf("bleu" not in list_available_metrics(), "`bleu` is not available!")
-    def test_bleu(self):
-        metric = build_metric("bleu")
-        pred_1 = ["It", "is", "a", "guide", "to", "action", "which", "ensures", "that", "the", "military", "always",
-                  "obeys", "the", "commands", "of", "the", "party"]
-        ref_1a = ["It", "is", "a", "guide", "to", "action", "that", "ensures", "that", "the", "military", "will",
-                  "forever", "heed", "Party", "commands"]
-        ref_1b = ["It", "is", "the", "guiding", "principle", "which", "guarantees", "the", "military", "forces",
-                  "always", "being", "under", "the", "command", "of", "the", "Party"]
-        ref_1c = ["It", "is", "the", "practical", "guide", "for", "the", "army", "always", "to", "heed", "the",
-                  "directions", "of", "the", "party"]
-        pred_2 = ["he", "read", "the", "book", "because", "he", "was", "interested", "in", "world", "history"]
-        ref_2a = ["he", "was", "interested", "in", "world", "history", "because", "he", "read", "the", "book"]
 
-        references = [[ref_1a, ref_1b, ref_1c], [ref_2a]]
-        predictions = [pred_1, pred_2]
+@pytest.mark.skipif("bleu" not in list_available_metrics(), reason="`bleu` is not available!")
+def test_bleu():
+    metric = build_metric("bleu")
+    pred_1 = ["It", "is", "a", "guide", "to", "action", "which", "ensures", "that", "the", "military", "always",
+              "obeys", "the", "commands", "of", "the", "party"]
+    ref_1a = ["It", "is", "a", "guide", "to", "action", "that", "ensures", "that", "the", "military", "will",
+              "forever", "heed", "Party", "commands"]
+    ref_1b = ["It", "is", "the", "guiding", "principle", "which", "guarantees", "the", "military", "forces",
+              "always", "being", "under", "the", "command", "of", "the", "Party"]
+    ref_1c = ["It", "is", "the", "practical", "guide", "for", "the", "army", "always", "to", "heed", "the",
+              "directions", "of", "the", "party"]
+    pred_2 = ["he", "read", "the", "book", "because", "he", "was", "interested", "in", "world", "history"]
+    ref_2a = ["he", "was", "interested", "in", "world", "history", "because", "he", "read", "the", "book"]
 
-        results = metric.compute(predictions, references)
+    references = [[ref_1a, ref_1b, ref_1c], [ref_2a]]
+    predictions = [pred_1, pred_2]
 
-        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
-        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
+    results = metric.compute(predictions, references)
 
-    @skipIf("cer" not in list_available_metrics(), "`cer` is not available!")
-    def test_cer(self):
-        metric = build_metric("cer")
-        predictions = ["بهتره این یکی رو بیخیال شیم"]
-        references = ["بهتره این یکی رو بیخیال بشیم"]
-        results = metric.compute(predictions, references)
-        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
-        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
+    assert isinstance(results, dict), INVALID_OUTPUT_TYPE
+    assert set(results.keys()) == set(metric.config.output_keys), INVALID_FIELDS
 
-    @skipIf("f1" not in list_available_metrics(), "`f1` is not available!")
-    def test_f1(self):
-        metric = build_metric("f1")
-        predictions = [0, 1, 2, 0, 1, 2]
-        references = [0, 2, 1, 0, 0, 1]
-        results = metric.compute(predictions, references)
-        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
-        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
 
-    @skipIf("precision" not in list_available_metrics(), "`precision` is not available!")
-    def test_precision(self):
-        metric = build_metric("precision")
-        predictions = [0, 1, 2, 0, 1, 2]
-        references = [0, 2, 1, 0, 0, 1]
-        results = metric.compute(predictions, references)
-        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
-        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
+@pytest.mark.skipif("cer" not in list_available_metrics(), reason="`cer` is not available!")
+def test_cer():
+    metric = build_metric("cer")
+    predictions = ["بهتره این یکی رو بیخیال شیم"]
+    references = ["بهتره این یکی رو بیخیال بشیم"]
+    results = metric.compute(predictions, references)
+    assert isinstance(results, dict), INVALID_OUTPUT_TYPE
+    assert set(results.keys()) == set(metric.config.output_keys), INVALID_FIELDS
 
-    @skipIf("recall" not in list_available_metrics(), "`recall` is not available!")
-    def test_recall(self):
-        metric = build_metric("recall")
-        predictions = [0, 1, 2, 0, 1, 2]
-        references = [0, 2, 1, 0, 0, 1]
-        results = metric.compute(predictions, references)
-        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
-        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
 
-    @skipIf("rouge" not in list_available_metrics(), "`rouge` is not available!")
-    def test_rouge(self):
-        metric = build_metric("rouge")
-        predictions = ["از این معیار برای سنجش خروجی مدل های ترجمه یا خلاصه سازی استفاده می شود"]
-        references = ["در واقع از این معیار برای سنجش خروجی مدل های ترجمه یا خلاصه سازی استفاده می شود"]
-        results = metric.compute(predictions, references)
-        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
-        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
+@pytest.mark.skipif("f1" not in list_available_metrics(), reason="`f1` is not available!")
+def test_f1():
+    metric = build_metric("f1")
+    predictions = [0, 1, 2, 0, 1, 2]
+    references = [0, 2, 1, 0, 0, 1]
+    results = metric.compute(predictions, references)
+    assert isinstance(results, dict), INVALID_OUTPUT_TYPE
+    assert set(results.keys()) == set(metric.config.output_keys), INVALID_FIELDS
 
-    @skipIf("seqeval" not in list_available_metrics(), "`seqeval` is not available!")
-    def test_seqeval(self):
-        metric = build_metric("seqeval")
-        predictions = [['O', 'O', 'O', 'B-MISC', 'I-MISC', 'I-MISC', 'O'], ['B-PER', 'I-PER', 'O']]
-        references = [['O', 'O', 'B-MISC', 'I-MISC', 'I-MISC', 'I-MISC', 'O'], ['B-PER', 'I-PER', 'O']]
-        results = metric.compute(predictions, references)
-        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
-        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
 
-    @skipIf("wer" not in list_available_metrics(), "`wer` is not available!")
-    def test_wer(self):
-        metric = build_metric("wer")
-        predictions = ["بهتره این یکی رو بیخیال شیم"]
-        references = ["بهتره این یکی رو بیخیال بشیم"]
-        results = metric.compute(predictions, references)
-        self.assertEqual(type(results), dict, INVALID_OUTPUT_TYPE)
-        self.assertEqual(set(results.keys()), set(metric.config.output_keys), INVALID_FIELDS)
+@pytest.mark.skipif("precision" not in list_available_metrics(), reason="`precision` is not available!")
+def test_precision():
+    metric = build_metric("precision")
+    predictions = [0, 1, 2, 0, 1, 2]
+    references = [0, 2, 1, 0, 0, 1]
+    results = metric.compute(predictions, references)
+    assert isinstance(results, dict), INVALID_OUTPUT_TYPE
+    assert set(results.keys()) == set(metric.config.output_keys), INVALID_FIELDS
+
+
+@pytest.mark.skipif("recall" not in list_available_metrics(), reason="`recall` is not available!")
+def test_recall():
+    metric = build_metric("recall")
+    predictions = [0, 1, 2, 0, 1, 2]
+    references = [0, 2, 1, 0, 0, 1]
+    results = metric.compute(predictions, references)
+    assert isinstance(results, dict), INVALID_OUTPUT_TYPE
+    assert set(results.keys()) == set(metric.config.output_keys), INVALID_FIELDS
+
+
+@pytest.mark.skipif("rouge" not in list_available_metrics(), reason="`rouge` is not available!")
+def test_rouge():
+    metric = build_metric("rouge")
+    predictions = ["از این معیار برای سنجش خروجی مدل های ترجمه یا خلاصه سازی استفاده می شود"]
+    references = ["در واقع از این معیار برای سنجش خروجی مدل های ترجمه یا خلاصه سازی استفاده می شود"]
+    results = metric.compute(predictions, references)
+    assert isinstance(results, dict), INVALID_OUTPUT_TYPE
+    assert set(results.keys()) == set(metric.config.output_keys), INVALID_FIELDS
+
+
+@pytest.mark.skipif("seqeval" not in list_available_metrics(), reason="`seqeval` is not available!")
+def test_seqeval():
+    metric = build_metric("seqeval")
+    predictions = [['O', 'O', 'O', 'B-MISC', 'I-MISC', 'I-MISC', 'O'], ['B-PER', 'I-PER', 'O']]
+    references = [['O', 'O', 'B-MISC', 'I-MISC', 'I-MISC', 'I-MISC', 'O'], ['B-PER', 'I-PER', 'O']]
+    results = metric.compute(predictions, references)
+    assert isinstance(results, dict), INVALID_OUTPUT_TYPE
+    assert set(results.keys()) == set(metric.config.output_keys), INVALID_FIELDS
+
+
+@pytest.mark.skipif("wer" not in list_available_metrics(), reason="`wer` is not available!")
+def test_wer():
+    metric = build_metric("wer")
+    predictions = ["بهتره این یکی رو بیخیال شیم"]
+    references = ["بهتره این یکی رو بیخیال بشیم"]
+    results = metric.compute(predictions, references)
+    assert isinstance(results, dict), INVALID_OUTPUT_TYPE
+    assert set(results.keys()) == set(metric.config.output_keys), INVALID_FIELDS
