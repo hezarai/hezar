088098b3dad11a3b160b1729ed135beb7d20601e
arxyzan <arxyzan@gmail.com>
2023-11-28 15:06:27 +0330

    :ambulance: Fix embedding config bug

--- hezar/embeddings/embedding.py
+++ hezar/embeddings/embedding.py
@@ -59,6 +59,7 @@ class Embedding:
 
     def __init__(self, config: EmbeddingConfig, embedding_file: str = None, vectors_file: str = None, **kwargs):
         verify_dependencies(self, self.required_backends)  # Check if all the required dependencies are installed
+        self.config = config.update(kwargs)
         if not self.config.bypass_version_check:
             _verify_gensim_installation()
 
