8fbb32fd1455d79266c39d647a29aa16303857f3
arxyzan <arxyzan@gmail.com>
2023-12-17 20:02:23 +0330

    :sparkles: Add ViT to backbones

---
+++ hezar/models/backbone/vit/__init__.py
@@ -0,0 +1,2 @@
+from .vit import ViT
+from .vit_config import ViTConfig
---
+++ hezar/models/backbone/vit/vit_config.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+from ....configs import ModelConfig
+
+
+@dataclass
+class ViTConfig(ModelConfig):
+    name = "vit"
+    hidden_size: int = 768
+    num_hidden_layers: int = 12
+    num_attention_heads: int = 12
+    intermediate_size: int = 3072
+    hidden_act: str = "gelu"
+    hidden_dropout_prob: float = 0.0
+    attention_probs_dropout_prob: float = 0.0
+    initializer_range: float = 0.02
+    layer_norm_eps: float = 1e-12
+    image_size: int = 224
+    patch_size: int = 16
+    num_channels: int = 3
+    qkv_bias: bool = True
+    encoder_stride: int = 16
