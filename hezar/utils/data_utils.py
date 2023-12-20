7c60416b2091ffc7febc7879bab1cd93e9a29928
arxyzan <arxyzan@gmail.com>
2023-12-20 11:51:10 +0330

    :sparkles: Add `shift_tokens_right()` to utils

--- hezar/utils/data_utils.py
+++ hezar/utils/data_utils.py
@@ -12,6 +12,7 @@ __all__ = [
     "convert_batch_dict_dtype",
     "resolve_inputs_length_for_padding",
     "pad_batch_items",
+    "shift_tokens_right",
     "get_non_numeric_keys",
     "flatten_dict",
 ]
@@ -152,6 +153,20 @@ def pad_batch_items(
             padded_inputs.append(ids[:inputs_length])
 
     return padded_inputs
+
+
+def shift_tokens_right(input_ids: "torch.Tensor", pad_token_id: int, decoder_start_token_id: int):
+    """
+    Shift input ids one token to the right.
+    """
+    shifted_input_ids = input_ids.new_zeros(input_ids.shape)
+    shifted_input_ids[:, 1:] = input_ids[:, :-1].clone()
+    shifted_input_ids[:, 0] = decoder_start_token_id
+
+    # replace possible -100 values in labels by `pad_token_id`
+    shifted_input_ids.masked_fill_(shifted_input_ids == -100, pad_token_id)
+
+    return shifted_input_ids
 
 
 def get_non_numeric_keys(d: Dict, batched=True):
