35bf20f83e1537e13da071e2eb7c5cb808b05153
arxyzan <arxyzan@gmail.com>
2023-12-20 11:53:39 +0330

    :hammer: Complete `train_image_captioning.py`

--- examples/train/train_image_captioning.py
+++ examples/train/train_image_captioning.py
@@ -1,44 +1,24 @@
-from hezar.models import ViTRobertaImage2Text, ViTRobertaImage2TextConfig, build_vision_encoder_decoder_model, Model
-from hezar.preprocessors import Preprocessor, ImageProcessor, ImageProcessorConfig
+from hezar.models import Model
 from hezar.data import Dataset
 from hezar.trainer import Trainer, TrainerConfig
-from hezar.utils import get_state_dict_from_hub
 
 
-base_decoder_path = "hezarai/roberta-base-fa"
-base_encoder_path = "vit-base-patch16-224"
+base_model_path = "hezarai/vit-roberta-fa-base"
 
-train_dataset = Dataset.load("hezarai/flickr30k-fa", split="train")
-eval_dataset = Dataset.load("hezarai/flickr30k-fa", split="test")
+train_dataset = Dataset.load("hezarai/flickr30k-fa", split="train", tokenizer_path=base_model_path, max_length=128)
+eval_dataset = Dataset.load("hezarai/flickr30k-fa", split="test", tokenizer_path=base_model_path, max_length=128)
 
-model = ViTRobertaImage2Text(ViTRobertaImage2TextConfig())
-encoder = Model.load(base_encoder_path)
-decoder = Model.load(base_decoder_path)
-vision_encoder_decoder = build_vision_encoder_decoder_model(encoder, decoder)
-
-model.load_state_dict(vision_encoder_decoder.state_dict())
-
-tokenizer = Preprocessor.load(base_decoder_path)
-image_processor = ImageProcessor(
-    ImageProcessorConfig(
-        mean=[0.5],
-        std=[0.5],
-        rescale=1/255,
-        resample=2,
-        size=(224, 224),
-    )
-)
-model.preprocessor = [tokenizer, image_processor]
-
+model = Model.load(base_model_path)
 
 train_config = TrainerConfig(
-    output_dir="crnn-plate-fa-v1",
+    output_dir="vit-roberta-fa-image-captioning-flickr30k",
     task="image2text",
     device="cuda",
-    batch_size=8,
+    batch_size=16,
     num_epochs=20,
-    metrics=["cer"],
-    metric_for_best_model="cer"
+    use_amp=True,
+    metrics=["wer"],
+    metric_for_best_model="wer"
 )
 
 trainer = Trainer(
