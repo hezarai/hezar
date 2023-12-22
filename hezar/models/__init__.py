33f16c04a5c7caf655b8d5d8523e66f2a0952780
arxyzan <arxyzan@gmail.com>
2023-12-22 12:49:23 +0330

    :fire: Remove empty models modules

--- hezar/models/__init__.py
+++ hezar/models/__init__.py
@@ -1,12 +1,10 @@
 from ..registry import register_model  # noqa
 from .model import Model, ModelConfig  # noqa
-from .audio_classification import *
 from .backbone import *
 from .image2text import *
 from .language_modeling import *
 from .sequence_labeling import *
 from .speech_recognition import *
 from .text_classification import *
-from .text_detection import *
 from .text_embedding import *
 from .text_generation import *
