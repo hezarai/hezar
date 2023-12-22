7a81af8a129a17d6d2ed7cf1cc841a38926ef94d
arxyzan <arxyzan@gmail.com>
2023-12-22 12:49:47 +0330

    :fire: Remove support for direct importing of main classes

--- hezar/__init__.py
+++ hezar/__init__.py
@@ -1,57 +1,15 @@
-"""
-Direct importing from hezar's root is no longer supported nor recommended since version 0.33.0. The following is just a
-workaround for backward compatibility. Any class, functions, etc. must be imported from its main submodule under hezar.
-"""
-import warnings
+# Copyright 2023 Hezar AI Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-
-__version__ = "0.34.0"
-
-
-def _warn_on_import(name: str, submodule: str):
-    warnings.warn(
-        f"Importing {name} from hezar root is deprecated and will be removed soon. "
-        f"Please use `from {submodule} import {name}`"
-    )
-
-
-def __getattr__(name: str):
-    if name == "Model":
-        from hezar.models import Model
-        _warn_on_import(name, "hezar.models")
-        return Model
-    elif name == "Dataset":
-        from .data import Dataset
-        _warn_on_import(name, "hezar.data")
-        return Dataset
-    elif name == "Trainer":
-        from .trainer import Trainer
-        _warn_on_import(name, "hezar.trainer")
-        return Trainer
-    elif name == "Embedding":
-        from .embeddings import Embedding
-        _warn_on_import(name, "hezar.embeddings")
-        return Embedding
-    elif name == "Preprocessor":
-        from .preprocessors import Preprocessor
-        _warn_on_import(name, "hezar.preprocessors")
-        return Preprocessor
-    elif name == "Metric":
-        from .metrics import Metric
-        _warn_on_import(name, "hezar.metrics")
-        return Metric
-    elif "Config" in name:
-        from .configs import Config
-        _warn_on_import(name, "hezar.configs")
-        return Config
-
-
-__all__ = [
-    "Config",
-    "Model",
-    "Dataset",
-    "Trainer",
-    "Preprocessor",
-    "Embedding",
-    "Metric",
-]
+__version__ = "0.35.0"
