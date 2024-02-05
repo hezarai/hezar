bc3058814cc57c51c3e8370f67c6d02908b850af
arxyzan <arxyzan@gmail.com>
2024-02-05 14:37:13 +0330

    :pencil2: Update `train_speech_recognition.py`

--- examples/train/train_speech_recognition.py
+++ examples/train/train_speech_recognition.py
@@ -1,25 +1,29 @@
-from hezar.models import WhisperSpeechRecognition, WhisperSpeechRecognitionConfig
-from hezar.data import Dataset
-from hezar.preprocessors import Preprocessor
+from hezar.models import Model
+from hezar.data import SpeechRecognitionDataset, SpeechRecognitionDatasetConfig
 from hezar.trainer import Trainer, TrainerConfig
 
 
-dataset_path = "hezarai/commonvoice-13-fa"
-base_model_path = "hezarai/whisper-small-fa"
+dataset_path = "hezarai/common-voice-13-fa"
+base_model_path = "hezarai/whisper-small"
 
-train_dataset = Dataset.load(dataset_path, split="train", tokenizer_path=base_model_path)
-eval_dataset = Dataset.load(dataset_path, split="test", tokenizer_path=base_model_path)
-
-model = WhisperSpeechRecognition(WhisperSpeechRecognitionConfig())
-preprocessor = Preprocessor.load(base_model_path)
+dataset_config = SpeechRecognitionDatasetConfig(
+    path=dataset_path,
+    feature_extractor_path=base_model_path,
+    tokenizer_path=base_model_path,
+)
+train_dataset = SpeechRecognitionDataset(dataset_config, split="train", labels_max_length=64)
+eval_dataset = SpeechRecognitionDataset(dataset_config, split="test[:50%]", labels_max_length=64)
+model = Model.load(base_model_path)
 
 train_config = TrainerConfig(
     output_dir="whisper-small-fa-commonvoice",
     task="speech_recognition",
     init_weights_from=base_model_path,
-    batch_size=8,
+    mixed_precision="bf16",
+    gradient_accumulation_steps=8,
+    batch_size=4,
     num_epochs=5,
-    metrics=["cer"],
+    metrics=["cer", "wer"],
 )
 
 trainer = Trainer(
@@ -28,6 +32,5 @@ trainer = Trainer(
     train_dataset=train_dataset,
     eval_dataset=eval_dataset,
     data_collator=train_dataset.data_collator,
-    preprocessor=preprocessor,
 )
 trainer.train()
