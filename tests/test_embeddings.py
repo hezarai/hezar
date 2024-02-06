cbefb9a33f74cafb7a57031ae30d9c4822ce10a1
arxyzan <arxyzan@gmail.com>
2024-02-06 12:16:50 +0330

    :test_tube: Clean cache after every test

--- tests/test_embeddings.py
+++ tests/test_embeddings.py
@@ -1,6 +1,11 @@
+import os
+
 import pytest
 
 from hezar.embeddings import Embedding
+from hezar.utils import clean_cache
+
+CI_MODE = os.environ.get("CI_MODE", "FALSE")
 
 
 TESTABLE_EMBEDDINGS = {
@@ -12,16 +17,14 @@ MOST_SIMILAR_TOP_N = 5
 
 
 @pytest.mark.parametrize("embedding_key", TESTABLE_EMBEDDINGS.keys())
-def test_embedding_most_similar(embedding_key):
+def test_embeddings(embedding_key):
     embedding_model = Embedding.load(TESTABLE_EMBEDDINGS[embedding_key])
     most_similar = embedding_model.most_similar("هزار", top_n=MOST_SIMILAR_TOP_N)
     assert isinstance(most_similar, list)
     assert len(most_similar) == MOST_SIMILAR_TOP_N
     assert {k for x in most_similar for k in x.keys()} == {"word", "score"}
-
-
-@pytest.mark.parametrize("embedding_key", TESTABLE_EMBEDDINGS.keys())
-def test_embedding_doesnt_match(embedding_key):
-    embedding_model = Embedding.load(TESTABLE_EMBEDDINGS[embedding_key])
     doesnt_match = embedding_model.doesnt_match(["کامپیوتر", "مهندس", "نرم افزار", "دوغ"])
     assert isinstance(doesnt_match, str)
+
+    if CI_MODE == "TRUE":
+        clean_cache(delay=1)
