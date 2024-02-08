b85915d9b0dd3d87074dde8eb64148c6a937b2c9
arxyzan <arxyzan@gmail.com>
2024-02-08 10:42:04 +0330

    :pencil2: Minor change in examples

--- examples/inference/text_classification_example.py
+++ examples/inference/text_classification_example.py
@@ -1,6 +1,6 @@
 from hezar.models import Model
 
 example = ["هزار، کتابخانه‌ای کامل برای به کارگیری آسان هوش مصنوعی"]
-model = Model.load("hezarai/distilbert-fa-sentiment-dksf")
+model = Model.load("hezarai/roberta-fa-sentiment-dksf")
 outputs = model.predict(example)
 print(outputs)
