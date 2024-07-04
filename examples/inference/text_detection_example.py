a7187f0625ebec08b6029ff4e3e939ca506a8518
arxyzan <arxyzan@gmail.com>
2024-07-04 22:05:21 +0330

    :bento: Add example image for text detection

--- examples/inference/text_detection_example.py
+++ examples/inference/text_detection_example.py
@@ -4,7 +4,7 @@ from hezar.utils import draw_boxes
 
 
 model = Model.load("craft", device="cuda")
-outputs = model.predict("../assets/O1.png")
-image = cv2.imread("../assets/O1.png")
+outputs = model.predict("../assets/text_detection_example.png")
+image = cv2.imread("../assets/text_detection_example.png")
 result_image = draw_boxes(image, outputs[0]["boxes"])
 cv2.imwrite("detected.png", result_image)
