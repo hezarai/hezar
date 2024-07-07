2495f4f6791d690f1d9db9863189e33c64d896e5
arxyzan <arxyzan@gmail.com>
2024-07-07 11:24:11 +0330

    :pencil2: Fix image path in `text_detection_example.py`

--- examples/inference/text_detection_example.py
+++ examples/inference/text_detection_example.py
@@ -3,7 +3,7 @@ from hezar.utils import load_image, draw_boxes, show_image
 
 
 model = Model.load("hezarai/CRAFT", device="cuda")
-image = load_image("../assets/text_detection_example.jpg")
+image = load_image("../assets/text_detection_example.png")
 outputs = model.predict(image)
 result_image = draw_boxes(image, outputs[0]["boxes"])
 show_image(result_image, "text_detected")
