0fc6dd41ddfb57ae0e37e4231ef77d834a55628c
arxyzan <arxyzan@gmail.com>
2024-10-09 16:22:49 +0330

    :pencil2: Add `_torch_extract_fbank_features` to `WhisperFeatureExtractor`

--- hezar/models/speech_recognition/whisper/whisper_feature_extractor.py
+++ hezar/models/speech_recognition/whisper/whisper_feature_extractor.py
@@ -129,7 +129,7 @@ class WhisperFeatureExtractor(AudioFeatureExtractor):
         # make sure list is in array format
         input_features = padded_inputs.get("input_features").transpose(2, 0, 1)
 
-        input_features = [self._np_extract_fbank_features(waveform) for waveform in input_features[0]]
+        input_features = [self._torch_extract_fbank_features(waveform) for waveform in input_features[0]]
 
         if isinstance(input_features[0], List):
             padded_inputs["input_features"] = [np.asarray(feature, dtype=np.float32) for feature in input_features]
@@ -168,6 +168,36 @@ class WhisperFeatureExtractor(AudioFeatureExtractor):
         log_spec = np.maximum(log_spec, log_spec.max() - 8.0)
         log_spec = (log_spec + 4.0) / 4.0
         return log_spec
+
+    def _torch_extract_fbank_features(self, waveform: np.array, device: str = "cpu") -> np.ndarray:
+        """
+        Compute the log-mel spectrogram of the audio using PyTorch's GPU-accelerated STFT implementation with batching,
+        yielding results similar to cpu computing with 1e-5 tolerance.
+        """
+        waveform = torch.from_numpy(waveform).type(torch.float32)
+
+        window = torch.hann_window(self.config.n_fft)
+        if device != "cpu":
+            waveform = waveform.to(device)
+            window = window.to(device)
+        stft = torch.stft(waveform, self.config.n_fft, self.config.hop_length, window=window, return_complex=True)
+        magnitudes = stft[..., :-1].abs() ** 2
+
+        mel_filters = torch.from_numpy(self.mel_filters).type(torch.float32)
+        if device != "cpu":
+            mel_filters = mel_filters.to(device)
+        mel_spec = mel_filters.T @ magnitudes
+
+        log_spec = torch.clamp(mel_spec, min=1e-10).log10()
+        if waveform.dim() == 2:
+            max_val = log_spec.max(dim=2, keepdim=True)[0].max(dim=1, keepdim=True)[0]
+            log_spec = torch.maximum(log_spec, max_val - 8.0)
+        else:
+            log_spec = torch.maximum(log_spec, log_spec.max() - 8.0)
+        log_spec = (log_spec + 4.0) / 4.0
+        if device != "cpu":
+            log_spec = log_spec.detach().cpu()
+        return log_spec.numpy()
 
     @staticmethod
     def zero_mean_unit_var_norm(
