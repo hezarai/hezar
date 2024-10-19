8f1f0b2a0cb25ccd91c5119905da5a1320ed42e8
arxyzan <arxyzan@gmail.com>
2024-10-19 18:53:33 +0330

    :sparkles: Add text summarization dataset processor

--- hezar/data/dataset_processors/__init__.py
+++ hezar/data/dataset_processors/__init__.py
@@ -2,3 +2,4 @@ from .dataset_processor import DatasetProcessor
 from .image_captioning_processor import ImageCaptioningDatasetProcessor
 from .sequence_labeling_processor import SequenceLabelingDatasetProcessor
 from .text_classification_processor import TextClassificationDatasetProcessor
+from .text_summarization_processor import TextSummarizationDatasetProcessor
