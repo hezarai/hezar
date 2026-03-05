c7084388475767b487fb33f7e757b20fe066fc1c
arxyzan <arxyzan@gmail.com>
2026-03-05 13:30:05 +0330

    :bug: Fix deps versions for embeddings

--- hezar/configs.py
+++ hezar/configs.py
@@ -369,7 +369,7 @@ class EmbeddingConfig(Config):
     name: str = field(init=False, default=None)
     config_type: str = field(init=False, default=ConfigType.EMBEDDING)
     bypass_version_check: bool = field(
-        default=False,
+        default=True,
         metadata={"help": "Whether to bypass checking gensim/numpy/hezar version compatibility"},
     )
 
